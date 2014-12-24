b5fc7e4dc6ff1ae55ce6ec40cf69b7ca05467a58
manukyansirarpiptfly <manukyansirarpi.ptfly@gmail.com>
2014-12-24 23:57:29 +0400

    gulp setup

--- assets/gulpfile.js
+++ assets/gulpfile.js
@@ -1,21 +1,25 @@
-var gulp = require('gulp');
-var sass = require('gulp-ruby-sass');
-var prefix = require('gulp-autoprefixer');
-
-// gulp.task('default', function () {
-//     gulp.src('src/css/index.scss')
-//         .pipe(sass({sourcemap: true, style: 'compact'}))
-//         .pipe(prefix("last 1 version", "> 1%", "ie 8", "ie 7"))
-//         .pipe(gulp.dest('dist'));
-// });
+var gulp = require('gulp'),
+	sass = require('gulp-ruby-sass'),
+	prefix = require('gulp-autoprefixer'),
+	minifyCSS = require('gulp-minify-css'),
+	rename = require('gulp-rename'),
+	watch = require('gulp-watch');
 
 gulp.task('styles', function() {
-  return gulp.src('src/css/index.scss')
+  return gulp.src('src/css/*.scss')
     .pipe(sass({ style: 'expanded' }))
-    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-    .pipe(gulp.dest('dist/assets/css'))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(minifycss())
-    .pipe(gulp.dest('dist/assets/css'))
-    .pipe(notify({ message: 'Styles task complete' }));
+    .pipe(gulp.dest('dist/assets/css'));
 });
+
+gulp.task('minify-css', function() {
+   return gulp.src('dist/assets/css/*.css')
+    .pipe(minifyCSS({keepBreaks:true}))
+    .pipe(rename('main.min.css'))
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('watch', function() {
+    gulp.watch('src/css/*.scss', ['styles', 'minify-css']);
+});
+
+gulp.task('default', ['watch']);
