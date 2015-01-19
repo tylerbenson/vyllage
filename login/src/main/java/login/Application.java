35f3ddff82b8228faef25b341cce581ae080fb1d
Tyler Benson <tyler@newrelic.com>
2015-01-19 00:09:45 -0800

    Initial setup for login database

--- login/src/main/java/login/Application.java
+++ login/src/main/java/login/Application.java
@@ -1,16 +1,68 @@
 package login;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
-public class Application {
+public class Application implements CommandLineRunner {
+
+	@Autowired
+	private PersonRepository repository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.err.println(this.repository.findAll());
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+}
+
+@Repository
+interface PersonRepository extends CrudRepository<Person, Long> {
+
+}
+
+@Entity
+class Person {
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String firstName;
+	private String lastName;
+
+	public String getFirstName() {
+		return this.firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return this.lastName;
+	}
+
+	public void setLastName(String lastname) {
+		this.lastName = lastname;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [firstName=" + this.firstName + ", lastName="
+				+ this.lastName + "]";
 	}
 }
