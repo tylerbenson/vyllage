a9dd2f5b458f128d8f78e949bf852ae9009843cf
manukyansirarpi <manukyansirarpi.leanv@gmail.com>
2015-01-31 21:36:06 +0400

    Profile section reactJS integration , MAin & Edit mode

--- assets/src/javascript/profile.js
+++ assets/src/javascript/profile.js
@@ -2,41 +2,41 @@
 
 // -------------------------- main mode -------------------------- 
 
-var Headline = React.createClass({	
+var Headline = React.createClass({displayName: "Headline",	
 
     render: function() {
 		return (
-            <div className="paragraph">
-                <p className="headline">
-                    { this.props.profileData.firstName + " "
+            React.createElement("div", {className: "paragraph"}, 
+                React.createElement("p", {className: "headline"}, 
+                     this.props.profileData.firstName + " "
                       + this.props.profileData.middleName + " "
                       + this.props.profileData.lastName
-                    }
-                </p>
-            </div>
+                    
+                )
+            )
 		);
     }
 });
 
-var Tagline = React.createClass({ 
+var Tagline = React.createClass({displayName: "Tagline", 
 
     render: function() {
         return (
-            <div className="paragraph">
-                <p className="tagline">{this.props.profileData.tagline}</p>
-            </div>
+            React.createElement("div", {className: "paragraph"}, 
+                React.createElement("p", {className: "tagline"}, this.props.profileData.tagline)
+            )
         );
     }
 });
 
-var HeadlineContainerMain = React.createClass({
+var HeadlineContainerMain = React.createClass({displayName: "HeadlineContainerMain",
 
     render: function() {
         return (
-            <div className="headline-container main">
-                <Headline profileData={this.props.profileData}/>
-                <Tagline profileData={this.props.profileData} />
-            </div>
+            React.createElement("div", {className: "headline-container main"}, 
+                React.createElement(Headline, {profileData: this.props.profileData}), 
+                React.createElement(Tagline, {profileData: this.props.profileData})
+            )
         );
     }
 });
@@ -44,7 +44,7 @@ var HeadlineContainerMain = React.createClass({
 
 // -------------------------- edit mode -------------------------- 
 
-var HeadlineEdit = React.createClass({  
+var HeadlineEdit = React.createClass({displayName: "HeadlineEdit",  
 
     getInitialState: function() {
         return {value:'Nathan M Benson'}; // this will be changed to get data from props: TBD
@@ -62,13 +62,13 @@ var HeadlineEdit = React.createClass({
         var value = this.state.value;
 
         return (
-            <input type="text" className="headline" placeholder="name, surname" 
-                value= {value} onChange={this.handleChange} />         
+            React.createElement("input", {type: "text", className: "headline", placeholder: "name, surname", 
+                value: value, onChange: this.handleChange})         
         );
     }
 });
 
-var TaglineEdit = React.createClass({ 
+var TaglineEdit = React.createClass({displayName: "TaglineEdit", 
 
     getInitialState: function() {
         return {value: 'Technology Enthusiast analyzing, building, and expanding solutions'}; // this will be changed to get data from props: TBD
@@ -86,15 +86,15 @@ var TaglineEdit = React.createClass({
         var value = this.state.value;
 
         return (
-            <input type="text" className="tagline"
-                placeholder="add a professional tagline" 
-                value = {value} 
-                onChange={this.handleChange} />
+            React.createElement("input", {type: "text", className: "tagline", 
+                placeholder: "add a professional tagline", 
+                value: value, 
+                onChange: this.handleChange})
         );
     }
 });
 
-var HeadlineContainerEdit = React.createClass({  
+var HeadlineContainerEdit = React.createClass({displayName: "HeadlineContainerEdit",  
 
     handleChange: function (tagline, isHeadline){
         if (this.props.onChange) {
@@ -104,10 +104,10 @@ var HeadlineContainerEdit = React.createClass({
 
     render: function() {
         return (
-            <div className="headline-container edit">
-                <HeadlineEdit profileData={this.props.profileData} onChange={this.handleChange }/>
-                <TaglineEdit profileData={this.props.profileData} onChange={this.handleChange } />
-            </div>
+            React.createElement("div", {className: "headline-container edit"}, 
+                React.createElement(HeadlineEdit, {profileData: this.props.profileData, onChange: this.handleChange}), 
+                React.createElement(TaglineEdit, {profileData: this.props.profileData, onChange: this.handleChange})
+            )
         );
     }
 });
@@ -115,7 +115,7 @@ var HeadlineContainerEdit = React.createClass({
 
 // -------------------------- main container for both modes -------------------------- 
 
-var HeadlineContainer = React.createClass({ 
+var HeadlineContainer = React.createClass({displayName: "HeadlineContainer", 
 
     getInitialState: function() {
         return {profileData: []};
@@ -151,10 +151,10 @@ var HeadlineContainer = React.createClass({
 
     render: function() {
         return (
-            <div>
-                <HeadlineContainerMain profileData={this.state.profileData} />
-                <HeadlineContainerEdit profileData={this.state.profileData} onChange={this.handleChange}/>
-            </div>
+            React.createElement("div", null, 
+                React.createElement(HeadlineContainerMain, {profileData: this.state.profileData}), 
+                React.createElement(HeadlineContainerEdit, {profileData: this.state.profileData, onChange: this.handleChange})
+            )
         );
     }
 
@@ -169,6 +169,6 @@ var Data = {
     tagline: 'Technology Enthusiast analyzing, building, and expanding solutions'
 };
 
-React.render(<HeadlineContainer />, document.getElementById('headline-container'));
+React.render(React.createElement(HeadlineContainer, null), document.getElementById('headline-container'));
 
 
