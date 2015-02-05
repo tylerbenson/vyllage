1c2c3e90c32b71e05f04caef332f7f3c3e099e9b
uh <carlos.uh@gmail.com>
2015-02-05 16:05:59 -0300

    Added post method for section and header.

--- profile/src/main/java/profile/ResumeController.java
+++ profile/src/main/java/profile/ResumeController.java
@@ -2,9 +2,11 @@ package profile;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.logging.Logger;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,39 +18,61 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Controller
 public class ResumeController {
+	Logger logger = Logger.getLogger(ResumeController.class.getName());
 
-	 @RequestMapping(value = "resume", method = RequestMethod.GET)
-	 public String resume(){
-		 
-		 return "redirect:/resume/1";
-	 }
-	 
+	@RequestMapping(value = "resume", method = RequestMethod.GET)
+	public String resume() {
+
+		return "redirect:/resume/1";
+	}
+
+	@RequestMapping(value = "resume/{resumeId}", method = RequestMethod.GET)
+	public String getResume(@PathVariable final Integer resumeId) {
+
+		return "resume";
+	}
+
+	@RequestMapping(value = "resume/{resumeId}/section", method = RequestMethod.GET, produces = "application/json")
+	public @ResponseBody String getResumeSection(
+			@PathVariable final Integer resumeId)
+			throws JsonProcessingException, IOException {
+		
+		ObjectMapper mapper = new ObjectMapper();
+		JsonFactory jfactory = new JsonFactory();
+
+		InputStream in = getClass().getResourceAsStream("resume-55-section(get).json");
+		JsonParser jParser = jfactory.createParser(in);
+
+		return mapper.readTree(jParser).toString();
+	}
+
+	@RequestMapping(value = "resume/{resumeId}/header", method = RequestMethod.GET, produces = "application/json")
+	public @ResponseBody String getResumeHeader(
+			@PathVariable final Integer resumeId)
+			throws JsonProcessingException, IOException {
+		
+		ObjectMapper mapper = new ObjectMapper();
+		JsonFactory jfactory = new JsonFactory();
+
+		InputStream in = getClass().getResourceAsStream("resume-resumeID-header.json");
+		JsonParser jParser = jfactory.createParser(in);
+
+		return mapper.readTree(jParser).toString();
+	}
 	
-	 @RequestMapping( value = "resume/{resumeId}" , method = RequestMethod.GET)
-	 public String getResume(@PathVariable String resumeId){
-		 
-		 return "resume";
-	 }
-	 
-	 @RequestMapping(value = "resume/{resumeId}/section", method = RequestMethod.GET, produces= "application/json")
-	 public @ResponseBody String getResumeSection(@PathVariable Integer resumeId) throws JsonProcessingException, IOException{
-		 ObjectMapper mapper = new ObjectMapper();
-		 JsonFactory jfactory = new JsonFactory();
-		 
-		 InputStream in = getClass().getResourceAsStream("resume-55-section(get).json");
-		 JsonParser jParser = jfactory.createParser(in);
-		 
-		 return mapper.readTree(jParser).toString();
-	 }
-	 
-	 @RequestMapping(value = "resume/{resumeId}/header", method = RequestMethod.GET, produces= "application/json")
-	 public @ResponseBody String getResumeHeader(@PathVariable Integer resumeId) throws JsonProcessingException, IOException{
-		 ObjectMapper mapper = new ObjectMapper();
-		 JsonFactory jfactory = new JsonFactory();
-		 
-		 InputStream in = getClass().getResourceAsStream("resume-resumeID-header.json");
-		 JsonParser jParser = jfactory.createParser(in);
-		 
-		 return mapper.readTree(jParser).toString();
-	 }
+	//TODO: Replace string with the actual object later
+	@RequestMapping(value = "resume/{resumeId}/section", method = RequestMethod.POST, consumes = "application/json")
+	public @ResponseBody String receiveSection(@RequestBody final String body) { 
+		logger.info(body);
+		
+		return body;
+	}
+	
+	//TODO: Replace string with the actual object later
+	@RequestMapping(value = "resume/{resumeId}/header", method = RequestMethod.POST, consumes = "application/json")
+	public @ResponseBody String receiveHeader(@RequestBody final String body) { 
+		logger.info(body);
+			
+		return body;
+	}
 }
