875d73b9653911ff450ebfe9690f9f2002aea423
uh <carlos.uh@gmail.com>
2015-02-06 17:06:05 -0300

    More tests.

--- editor/src/test/java/editor/ApplicationTests.java
+++ editor/src/test/java/editor/ApplicationTests.java
@@ -1,5 +1,6 @@
 package editor;
 
+import static editor.domain.public_.tables.Accounts.ACCOUNTS;
 import static org.junit.Assert.assertEquals;
 
 import org.jooq.DSLContext;
@@ -11,8 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-
-import editor.domain.public_.tables.Accounts;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
@@ -29,18 +28,58 @@ public class ApplicationTests {
 	@Test
 	public void testUserNotFound() {
 		// DSLContext sql = DSL.using(dataSource, SQLDialect.H2);
-		Result<Record> records = sql.select().from(Accounts.ACCOUNTS)
-				.where(Accounts.ACCOUNTS.USERNAME.eq("invalidUser")).fetch();
+		Result<Record> records = sql.select().from(ACCOUNTS)
+				.where(ACCOUNTS.USERNAME.eq("invalidUser")).fetch();
 
 		assertEquals(records.isEmpty(), true);
 	}
 
 	@Test
 	public void testUserFound() {
-		Result<Record> records = sql.select().from(Accounts.ACCOUNTS)
-				.where(Accounts.ACCOUNTS.USERNAME.eq("username")).fetch();
+		Result<Record> records = sql.select().from(ACCOUNTS)
+				.where(ACCOUNTS.USERNAME.eq("username")).fetch();
 
 		assertEquals(records.isEmpty(), false);
 	}
 
+	@Test
+	public void testInsertUser() {
+		sql.insertInto(ACCOUNTS, ACCOUNTS.USERNAME).values("test").execute();
+
+		Result<Record> records = sql.select().from(ACCOUNTS)
+				.where(ACCOUNTS.USERNAME.eq("test")).fetch();
+
+		assertEquals("User not found ", records.isEmpty(), false);
+		assertEquals(records.get(0).getValue(ACCOUNTS.USERNAME), "test");
+	}
+
+	@Test
+	public void testUpdateUser() {
+		sql.insertInto(ACCOUNTS, ACCOUNTS.USERNAME).values("test").execute();
+
+		sql.update(ACCOUNTS).set(ACCOUNTS.USERNAME, "updated")
+				.where(ACCOUNTS.USERNAME.eq("test")).execute();
+
+		Result<Record> records = sql.select().from(ACCOUNTS)
+				.where(ACCOUNTS.USERNAME.eq("test")).fetch();
+
+		Result<Record> updatedRecords = sql.select().from(ACCOUNTS)
+				.where(ACCOUNTS.USERNAME.eq("updated")).fetch();
+
+		assertEquals("User not udated ", records.isEmpty(), true);
+		assertEquals(updatedRecords.get(0).getValue(ACCOUNTS.USERNAME),
+				"updated");
+	}
+
+	@Test
+	public void testDeleteUser() {
+		sql.insertInto(ACCOUNTS, ACCOUNTS.USERNAME).values("test").execute();
+
+		sql.delete(ACCOUNTS).where(ACCOUNTS.USERNAME.eq("test")).execute();
+
+		Result<Record> records = sql.select().from(ACCOUNTS)
+				.where(ACCOUNTS.USERNAME.eq("test")).fetch();
+
+		assertEquals("User not deleted ", records.isEmpty(), true);
+	}
 }
