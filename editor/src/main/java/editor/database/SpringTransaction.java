41600ae167e43922d1bda05ff5f0696e12d2893d
uh <carlos.uh@gmail.com>
2015-02-06 15:55:42 -0300

    Added database configuration for Jooq. Added a couple of tests to check Jooq.

---
+++ editor/src/main/java/editor/database/SpringTransaction.java
@@ -0,0 +1,12 @@
+package editor.database;
+
+import org.jooq.Transaction;
+import org.springframework.transaction.TransactionStatus;
+
+public class SpringTransaction implements Transaction {
+	final TransactionStatus tx;
+
+	SpringTransaction(TransactionStatus tx) {
+		this.tx = tx;
+	}
+}
--- editor/src/test/java/editor/ApplicationTests.java
+++ editor/src/test/java/editor/ApplicationTests.java
@@ -1,43 +1,46 @@
 package editor;
 
+import static org.junit.Assert.assertEquals;
+
+import org.jooq.DSLContext;
+import org.jooq.Record;
+import org.jooq.Result;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.IntegrationTest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+import editor.domain.public_.tables.Accounts;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
 @WebAppConfiguration
-@IntegrationTest("server.port:0")
 public class ApplicationTests {
 
-	// @Autowired
-	// private DataSource dataSource;
+	@Autowired
+	DSLContext sql;
 
 	@Test
 	public void contextLoads() {
 	}
 
-	// @Test
-	// public void testUserNotFound() {
-	// DSLContext sql = DSL.using(dataSource, SQLDialect.H2);
-	//
-	// Result<Record> records = sql.select().from(Accounts.ACCOUNTS)
-	// .where(Accounts.ACCOUNTS.USERNAME.eq("invalidUser")).fetch();
-	//
-	// assertEquals(records.isEmpty(), true);
-	// }
-	//
-	// @Test
-	// public void testUserFound() {
-	// DSLContext sql = DSL.using(dataSource, SQLDialect.H2);
-	//
-	// Result<Record> records = sql.select().from(Accounts.ACCOUNTS)
-	// .where(Accounts.ACCOUNTS.USERNAME.eq("username")).fetch();
-	//
-	// assertEquals(records.isEmpty(), false);
-	// }
+	@Test
+	public void testUserNotFound() {
+		// DSLContext sql = DSL.using(dataSource, SQLDialect.H2);
+		Result<Record> records = sql.select().from(Accounts.ACCOUNTS)
+				.where(Accounts.ACCOUNTS.USERNAME.eq("invalidUser")).fetch();
+
+		assertEquals(records.isEmpty(), true);
+	}
+
+	@Test
+	public void testUserFound() {
+		Result<Record> records = sql.select().from(Accounts.ACCOUNTS)
+				.where(Accounts.ACCOUNTS.USERNAME.eq("username")).fetch();
+
+		assertEquals(records.isEmpty(), false);
+	}
 
 }
