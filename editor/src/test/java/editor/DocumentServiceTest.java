ee6706c1815286fb1ccb030226a87198ca709725
uh <carlos.uh@gmail.com>
2015-02-09 16:20:31 -0300

    Test retrieve several sections.

--- editor/src/test/java/editor/DocumentServiceTest.java
+++ editor/src/test/java/editor/DocumentServiceTest.java
@@ -1,5 +1,8 @@
 package editor;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -52,6 +55,44 @@ public class DocumentServiceTest {
 
 		DocumentSection documentSection = service.getDocumentSection(
 				documentId, sectionId);
+
+		Assert.assertNotNull(documentSection);
+	}
+
+	@Test
+	public void loadSeveralDocumentSectionsTest()
+			throws DocumentSectionNotFoundException {
+		long documentId = 1;
+
+		String json1 = "{" + "\"type\": \"experience\","
+				+ "\"title\": \"job experience\"," + "\"sectionId\": 124,"
+				+ "\"sectionPosition\": 2," + "\"state\": \"shown\","
+				+ "\"organizationName\": \"DeVry Education Group\","
+				+ "\"organizationDescription\": \"Blah Blah Blah.\","
+				+ "\"role\": \"Manager, Local Accounts\","
+				+ "\"startDate\": \"September 2010\"," + "\"endDate\": \"\","
+				+ "\"isCurrent\": true,"
+				+ "\"location\": \"Portland, Oregon\","
+				+ "\"roleDescription\": \"Blah Blah Blah\","
+				+ "\"highlights\": \"I was in charge of...\"" + "}";
+
+		String json2 = "{" + "\"type\": \"experience\","
+				+ "\"title\": \"job experience\"," + "\"sectionId\": 124,"
+				+ "\"sectionPosition\": 2," + "\"state\": \"shown\","
+				+ "\"organizationName\": \"DeVry Education Group\","
+				+ "\"organizationDescription\": \"Blah Blah Blah.\","
+				+ "\"role\": \"Manager, Local Accounts\","
+				+ "\"startDate\": \"September 2010\"," + "\"endDate\": \"\","
+				+ "\"isCurrent\": true,"
+				+ "\"location\": \"Portland, Oregon\","
+				+ "\"roleDescription\": \"Blah Blah Blah\","
+				+ "\"highlights\": \"I was in charge of...\"" + "}";
+
+		Mockito.doReturn(Arrays.asList(json1, json2)).when(dsrepository)
+				.getDocumentSections(documentId);
+
+		List<DocumentSection> documentSection = service
+				.getDocumentSections(documentId);
 
 		Assert.assertNotNull(documentSection);
 	}
