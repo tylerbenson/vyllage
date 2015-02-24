beac5eabb308a544a125b8a76165d64e719a74db
Tyler Benson <tylerbenson@gmail.com>
2015-02-24 10:02:09 -0800

    Reformat using 2 space indentation.

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -2,28 +2,28 @@ var CommonsChunkPlugin = require("webpack/lib/optimize/CommonsChunkPlugin");
 var path = require('path');
 
 module.exports = {
-	debug: true,
-	devtool: '#inline-source-map',
-	entry: {
-		'main.js': './src/main.jsx',
-		'register.js': './src/register.jsx',
-        'requestAdvice.js': './src/requestAdvice.jsx'
-	},
-	output: {
-		path: path.join(__dirname, 'public', 'javascript'),
-		filename: '[name]'
-	},
-	module: {
-		loaders: [{
-			test: /\.jsx$/,
-			loaders: ['babel']
-		}, ],
-	},
-	resolve: {
-		alias: {},
-		extensions: ['', '.js', '.jsx']
-	},
-	plugins: [
-		new CommonsChunkPlugin('base.js'),
-	]
+  debug: true,
+  devtool: '#inline-source-map',
+  entry: {
+    'main.js': './src/main.jsx',
+    'register.js': './src/register.jsx',
+    'requestAdvice.js': './src/requestAdvice.jsx'
+  },
+  output: {
+    path: path.join(__dirname, 'public', 'javascript'),
+    filename: '[name]'
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx$/,
+      loaders: ['babel']
+    }, ],
+  },
+  resolve: {
+    alias: {},
+    extensions: ['', '.js', '.jsx']
+  },
+  plugins: [
+    new CommonsChunkPlugin('base.js'),
+  ]
 };
