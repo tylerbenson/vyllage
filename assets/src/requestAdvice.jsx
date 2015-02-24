d27929fc9653dc5953934989a106d14944e451ff
kiran <kiran@webrafter.com>
2015-02-24 07:41:11 +0530

    basic markup change for new request advice requirement

--- assets/src/requestAdvice.jsx
+++ assets/src/requestAdvice.jsx
@@ -1,18 +1,26 @@
 var React = require('react');
 
+var data = ['John Smith', 'Edward Johnson', 'Cindy Lawson', 'Julie Kim', 'Sarah Shore', 'Sally Ride'];
+
 var RequestAdvice = React.createClass({
     render: function () {
         return (
-            <div className="rcpent-list" contentEditable>
-                <p className="rcpent">John Smith</p>
-                <p className="rcpent">Edward Johnson</p>
-                <p className="rcpent"> Cindy Lawson</p>
-                <p className="rcpent">Julie Kim</p>
-                <p className="rcpent">Sarah Shore</p>
-                <p className="rcpent"> Sally Ride</p>
+            <div className="row">
+              <div className="twelve columns">
+                  <div><p className="rqst-key-word">to:</p></div>
+                  <div>
+                    <div className='rcpent-form'>form</div>
+                    <div className='rcpent-list'>list</div>
+                  </div>
+                  <div className="fb-button">
+                    <p className="small-text">ask your</p>
+                    <p className="big-text">facebook</p>
+                    <p className="small-text">friends</p>
+                  </div>
+              </div>
             </div>
         );
     }
 });
 
-React.render(<RequestAdvice />, document.getElementById('rcpent-list'));
+React.render(<RequestAdvice />, document.getElementById('rcpents'));
