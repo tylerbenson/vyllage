dccf81356610e6556e8a12766ce56a3a792ca1d9
kiran <kiran@webrafter.com>
2015-03-01 16:48:30 +0530

    Made request advice body editable

--- assets/src/components/requestAdvice/FormBody.jsx
+++ assets/src/components/requestAdvice/FormBody.jsx
@@ -1,16 +1,21 @@
 var React = require('react');
+var ContentEditable = require("react-contenteditable");
+
+var defaultBody = '';
+defaultBody += '<p> I could really use your assistance on giving me some career or resume advice. Do you think you could take a couple of minutes and look over this for me? </p>';
+defaultBody += '<br/><p>Thanks,</p><p>Nathan</p>';
 
 var FormBody = React.createClass({
+  getInitialState: function(){
+    return {html: defaultBody};
+  },
+  handleChange: function(e){
+    this.setState({html: e.target.value});
+  },
   render: function () {
     return (
       <div className = "messageContent">
-        <p className="message-text">
-          I could really use your assistance on giving me some career or resume advice. Do you think
-          you could take a couple of minutes and look over this for me?
-        </p>
-        <br/>
-        <p className="message-text">Thanks,</p>
-        <p className="message-text">Nathan</p>
+        <ContentEditable html={this.state.html} onChange={this.handleChange}/>
       </div>
     );
   }
