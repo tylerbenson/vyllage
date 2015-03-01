ec324be49b83ece536c10ad1f9f85db3945b4f44
kiran <kiran@webrafter.com>
2015-03-01 16:42:18 +0530

    Made subject editable

--- assets/src/components/requestAdvice/FormSubject.jsx
+++ assets/src/components/requestAdvice/FormSubject.jsx
@@ -1,14 +1,19 @@
 var React = require('react');
+var ContentEditable = require("react-contenteditable");
 
 var FormSubject = React.createClass({
+  getInitialState: function(){
+    return {html: '<p>Could you provide me some feedback on my resume?</p>'};
+  },
+  handleChange: function(e){
+    this.setState({html: e.target.value});
+  },
   render: function () {
     return (
       <div className='row'>
         <p className="two columns rqst-key-word">subject:</p>
-        <div className="subject" >
-          <p className="subject-text">
-            Could you provide me some feedback on my resume?
-          </p>
+        <div className='subject'>
+          <ContentEditable html={this.state.html} onChange={this.handleChange}/>
         </div>
       </div>
     );
