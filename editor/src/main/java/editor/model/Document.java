bf8daa183515f656d51725f10555ce6367939237
uh <carlos.uh@gmail.com>
2015-03-02 08:06:31 -0300

    Merged.

--- editor/src/main/java/editor/model/Document.java
+++ editor/src/main/java/editor/model/Document.java
@@ -7,12 +7,8 @@ import lombok.ToString;
 @ToString
 public class Document {
 	private Long id;
-<<<<<<< Upstream, based on origin/master
 	private String tagline;
-	private Account account;
-=======
 	private Long userId;
->>>>>>> ad225a6 Eliminated Account from Editor. Added UserId to Document. Added FirstName, MiddleName, LastName to LOGIN.Users Deleted JooqTest as it's no longer required.
 	private Boolean visibility;
 	private LocalDateTime dateCreated;
 	private LocalDateTime lastModified;
