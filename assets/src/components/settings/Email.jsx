cfba0a25d9772091fa7da1fa24e79c0451c7a411
kiran <kiran@webrafter.com>
2015-03-10 17:16:49 +0530

    Fix for making email editable

--- assets/src/components/settings/Email.jsx
+++ assets/src/components/settings/Email.jsx
@@ -1,10 +1,14 @@
 var React = require('react');
 var PrivacySelect = require('./PrivacySelect');
 var Actions = require('./actions');
+var validator = require('validator');
 
 var Email = React.createClass({
   getInitialState: function () {
-    return { edit: false };
+    return { 
+      edit: false,
+      error:false
+    };
   },
   editHandler: function (e) {
     e.preventDefault();
@@ -12,16 +16,18 @@ var Email = React.createClass({
   },
   valueHandler: function (e) {
     e.preventDefault();
-    Actions.changeSettings('email', {value: e.target.value, privacy: this.props.privacy});
+    Actions.changeSetting('email', {value: e.target.value, privacy: this.props.privacy});
   },
   privacyHandler: function (e) {
     e.preventDefault();
-    Actions.changeSettings('email', {value: this.props.value, privacy: e.target.value});
+    Actions.changeSetting('email', {value: this.props.value, privacy: e.target.value});
   },
   keyPress: function (e) {
     e.stopPropagation();
     if (e.key === 'Enter') {
-      this.setState({edit: false});
+      this.setState({
+        edit: false
+      });
     }
   },
   renderForm: function () {
