6332e6ff509639eaa3e9b33a4c9c8b88c5a6ef80
manukyansirarpi <manukyansirarpi.leanv@gmail.com>
2015-03-17 15:57:59 +0400

    Resume Contact Info section

--- assets/src/components/profile/index.jsx
+++ assets/src/components/profile/index.jsx
@@ -1,87 +1,75 @@
 var React = require('react');
 var request = require('superagent');
-var ProfilePhotoContainer = require('./photo-container');
 var ArticleContent =  require('./article-content');
-var ShareContactButtons = require('./share-contact-btns');
 
 var ProfileContainer = React.createClass({ 
 
-    getInitialState: function() {
-        return {profileData: []};
-    },
+		getInitialState: function() {
+				return {profileData: []};
+		},
 
-    componentDidMount: function() {
+		componentDidMount: function() {
 
-        var self = this, documentId,
-            pathItems = window.location.pathname.split("/");
+				// var self = this, documentId,
+				// 		pathItems = window.location.pathname.split("/");
 
-        if(pathItems.length > 2) {
-            documentId = pathItems[2];
+				// if(pathItems.length > 2) {
+				// 		documentId = pathItems[2];
 
-            request
-               .get('/resume/' + documentId + '/header')
-               .set('Accept', 'application/json')
-               .end(function(error, res) {
+				// 		request
+				// 			 .get('/resume/' + documentId + '/header')
+				// 			 .set('Accept', 'application/json')
+				// 			 .end(function(error, res) {
 
-                    if (res.ok) {
-                        if(res.body.length == 0) {
-                            // if data from server is empty TBD??
-                        } else {
-                            self.setState({profileData: res.body});
-                        }
-                    } else {
-                        alert(res.text); // this is left intentionally 
-                        console.log(res.text);
-                   }             
-            });
-        } else {
-            // this case should not happen, throw error
-        }
+				// 						if (res.ok) {
+				// 								if(res.body.length == 0) {
+				// 										// if data from server is empty TBD??
+				// 								} else {
+				// 										self.setState({profileData: res.body});
+				// 								}
+				// 						} else {
+				// 								alert(res.text); // this is left intentionally 
+				// 								console.log(res.text);
+				// 					 }             
+				// 		});
+				// }
+		},
 
-    },
+		saveChanges: function (data) {
+						var self = this, documentId,
+						pathItems = window.location.pathname.split("/"),
+						token_header = document.getElementById('meta_header').content,
+						token_val = document.getElementById('meta_token').content;
 
-    saveChanges: function (data) {
-            var self = this, documentId,
-            pathItems = window.location.pathname.split("/"),
-            token_header = document.getElementById('meta_header').content,
-            token_val = document.getElementById('meta_token').content;
+				if(pathItems.length > 2) {
+						documentId = pathItems[2];
 
-        if(pathItems.length > 2) {
-            documentId = pathItems[2];
+						request
+								.post('/resume/' + documentId + '/header')
+								.set(token_header, token_val)
+								.send(data)
+								.end(function(error, res) {
 
-            request
-                .post('/resume/' + documentId + '/header')
-                .set(token_header, token_val)
-                .send(data)
-                .end(function(error, res) {
+										if (res.ok) {
+												 self.setState({profileData: data});
+										} else {
+											 alert( res.text );  // this is left intentionally
+											 console.log(res.text); 
+										}  
+								});
+				}
+		},
 
-                    if (res.ok) {
-                         self.setState({profileData: data});
-                    } else {
-                       alert( res.text );  // this is left intentionally
-                       console.log(res.text); 
-                    }  
-                });
-        }
-    },
-
-    render: function() {
-        return (
-            <div className="row">
-                <ProfilePhotoContainer />
-
-                <ArticleContent profileData={this.state.profileData} saveChanges={this.saveChanges}/>
-
-                <ShareContactButtons />
-            </div>
-        );
-    }
-
+		render: function() {
+				return (
+					<ArticleContent profileData={this.state.profileData} saveChanges={this.saveChanges}/>
+				);
+		}
 });
 
 // This render to id should be deleted after refractoring
-if (document.getElementById('profile')) {
-    React.render(<ProfileContainer />, document.getElementById('profile'));
+if (document.getElementById('resume-contactInfo')) {
+		React.render(<ProfileContainer />, document.getElementById('resume-contactInfo'));
 }
 
 module.exports = ProfileContainer;
