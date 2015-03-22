1f40ec8ce0f4ee6f4978fcd250863d1ababc9ded
manukyansirarpi <manukyansirarpi.leanv@gmail.com>
2015-03-22 19:29:48 +0400

    Added export menu

--- assets/src/components/header/primary-header.jsx
+++ assets/src/components/header/primary-header.jsx
@@ -2,27 +2,33 @@ var React = require('react');
 
 var PrimaryHeader = React.createClass({  
 
-    render: function() {
-      return (
-          <div className="header-top">
-            <div className="container">
-              <div className="row">
-                <div className="twelve columns">
-                  <div className="u-pull-left vyllage-logo">
-                    <p className="logo-title"> <span> Vyllage</span> <span className="logo-text">  Resume </span></p> 
-                  </div>
-                  <div className="u-pull-right header-controls">
-                    <span><i className="icon ion-ios-bell"></i></span>
-                    <span><i className="icon ion-gear-a"></i></span>
-                    <span className="header-img"><i className="icon ion-person"></i></span>
-                    <span className="username" onClick={this.handleClick}>Nathan B <span className="arrow-down"></span></span>
-                  </div>
-                </div>
+  accountMenu: function(){
+    if(this.props.accountMenu){
+      this.props.accountMenu();
+    }
+  },
+
+  render: function() {
+    return (
+      <div className="header-top">
+        <div className="container">
+          <div className="row">
+            <div className="twelve columns">
+              <div className="u-pull-left vyllage-logo">
+                <p className="logo-title"> <span> Vyllage</span> <span className="logo-text">  Resume </span></p> 
+              </div>
+              <div className="u-pull-right header-controls">
+                <span><i className="icon ion-ios-bell"></i></span>
+                <span><i className="icon ion-gear-a"></i></span>
+                <span className="header-img"><i className="icon ion-person"></i></span>
+                <span className="username" onClick={this.accountMenu}>Nathan B <span className="arrow-down"></span></span>
               </div>
             </div>
           </div>
-      );
-    }
+        </div>
+      </div>
+    );
+  }
 });
 
 module.exports = PrimaryHeader;
