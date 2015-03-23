e17f095ef72e0a7cf40f7feb0b1b9cf5c2d5e76e
kiran <kiran@webrafter.com>
2015-03-23 17:10:48 +0530

    Added comment icon

--- assets/src/components/resume/sections/Footer.jsx
+++ assets/src/components/resume/sections/Footer.jsx
@@ -7,12 +7,14 @@ var SectionFooter = React.createClass({
     actions.toggleComments(this.props.section.sectionId);
   },
   render: function () {
+    var commentsCount = null; // this.props.section && this.props.section.comments && this.props.section.comments.length;
     return (
       <div>
         <div className='row resume-section-footer'>
           <p className='u-pull-left'>Just updated now</p>
           <a className='u-pull-right' onClick={this.clickComments}>
-            
+            <i className='icon ion-chatbox comment-icon'></i>
+            <span className='comment-count'>{commentsCount}</span>
             comments
           </a> 
         </div>
