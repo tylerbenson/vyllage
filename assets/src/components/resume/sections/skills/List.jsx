0968119abc0ba8185201575165e764084265d116
kiran <kiran@webrafter.com>
2015-03-26 21:45:09 +0530

    Changes to list

--- assets/src/components/resume/sections/skills/List.jsx
+++ assets/src/components/resume/sections/skills/List.jsx
@@ -4,10 +4,15 @@ var SkillsList = React.createClass({
   render: function () {
     var sections = this.props.sections || [];
     var skillNodes = sections.map(function (section) {
-      return <span>{section.description}</span>;
+      return (
+        <div>
+          <span className='skill-item'>{section.description}</span>
+          <i className='icon ion-close'></i>
+        </div>
+      );
     });
     return (
-      <div className='row'>
+      <div className='u-pull-left'>
         {skillNodes}
       </div>
     );
