31311b4b064832776a1529302020f99afe3a3b30
kiran <kiran@webrafter.com>
2015-03-31 19:32:39 +0530

    Fixed initial vaule of career goals form not updating issue

--- assets/src/components/resume/sections/CareerGoal.jsx
+++ assets/src/components/resume/sections/CareerGoal.jsx
@@ -6,12 +6,10 @@ var SectionFooter = require('./Footer');
 
 var CareerGoal = React.createClass({
   render: function () {
-    console.log(this.props.careerGoal);
     return (
       <div className='section'>
         <div className='container'>
           <Freeform title='Career Goal' section={this.props.careerGoal} />
-          <SectionFooter section={this.props.careerGoal} />
         </div>
       </div>
     );
