adc7fdefedc9974f0d17aa6285281bca85800c2a
kiran <kiran@webrafter.com>
2015-04-05 00:09:45 +0530

    Hiding comments when clicked outside section didn't work as expected. need to try again

--- assets/src/components/resume/sections/Footer.jsx
+++ assets/src/components/resume/sections/Footer.jsx
@@ -5,13 +5,27 @@ var CommentsCount = require('../../buttons/comments-count');
 var moment = require('moment');
 
 var SectionFooter = React.createClass({
-  clickComments: function () {
+  stopPropagation: function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+  },
+  clickComments: function (e) {
     actions.toggleComments(this.props.section.sectionId);
   },
+  hideComments: function () {
+    actions.hideComments(this.props.section.sectionId);
+  },
+  // componentDidMount: function () {
+  //   var html = document.querySelector('html');
+  //   html.addEventListener('click', this.hideComments);
+  // },
+  // componentWillUnmount: function () {
+  //   document.body.removeEventListener('click', this.hideComments);
+  // },
   render: function () {
     var numberOfComments = this.props.section && this.props.section.numberOfComments;
     return (
-      <div className='footer'>
+      <div className='footer' onClick={this.stopPropagation}>
         <div className='content'>
           <p className='timestamp'>{moment(this.props.section.lastModified).fromNow()}</p>
           <div className='actions'>
