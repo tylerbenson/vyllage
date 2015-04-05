af63b6c7714a5ea43a9255cf8dc089a6e68ce8b6
kiran <kiran@webrafter.com>
2015-04-05 13:17:32 +0530

    Added footer to askadvice page

--- assets/src/askAdvice.jsx
+++ assets/src/askAdvice.jsx
@@ -1,5 +1,6 @@
 var React = require('react');
 var Header = require('./components/header');
+var Footer = require('./components/footer');
 var AskAdvice = require('./components/askAdvice');
 
 React.render(<AskAdvice />, document.getElementById('ask-advice'));
