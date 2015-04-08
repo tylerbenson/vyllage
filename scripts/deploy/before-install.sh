005b2abf978b0dbbaf9920ac53658440db40f1d3
Tyler Benson <tylerbenson@gmail.com>
2015-04-08 13:22:51 -0700

    Reorder install commands

--- scripts/deploy/before-install.sh
+++ scripts/deploy/before-install.sh
@@ -3,9 +3,11 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd -P "$( dirname "$SOURCE" )" && pwd )"
 sudo cp $DIR/../supervisord.conf /etc/supervisor/
 sudo cp $DIR/../vyllage.conf /etc/supervisor/conf.d/
-/usr/bin/supervisorctl reread
 
 sudo mkdir -p /opt/vyllage/log/
-sudo chmod -R 777 vyllage
+sudo chmod -R 777 /opt/vyllage
+
+/usr/bin/supervisorctl reread
+
 touch /opt/vyllage/blank.jar
 rm /opt/vyllage/*.jar
