102e33b33efbccd6e120ef8e2644007cb32c00e7
Tyler Benson <tylerbenson@gmail.com>
2015-04-08 12:35:25 -0700

    Adjust deploy process

--- scripts/deploy/after-install.sh
+++ scripts/deploy/after-install.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd -P "$( dirname "$SOURCE" )" && pwd )"
-
-mkdir -p /opt/vyllage/log/
-touch /opt/vyllage/blank.jar
-rm /opt/vyllage/*.jar
-cp $DIR/../../site-*.jar /opt/vyllage/
 ln -s /opt/vyllage/site-*.jar /opt/vyllage/site.jar
--- scripts/deploy/before-install.sh
+++ scripts/deploy/before-install.sh
@@ -4,3 +4,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd -P "$( dirname "$SOURCE" )" 
 sudo cp $DIR/../supervisord.conf /etc/supervisor/
 sudo cp $DIR/../vyllage.conf /etc/supervisor/conf.d/
 /usr/bin/supervisorctl reread
+
+sudo mkdir -p /opt/vyllage/log/
+sudo chmod -R 777 vyllage
+touch /opt/vyllage/blank.jar
+rm /opt/vyllage/*.jar
