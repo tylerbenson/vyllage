c5cf2d57141c4f6223525ae4b6ec0bf6f1542726
Tyler Benson <tylerbenson@gmail.com>
2015-04-07 22:32:48 -0700

    Reduce deployment size
    Configured to only copy site.jar into deployment folder.

---
+++ scripts/instance-setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#RUN as sudo.
+add-apt-repository ppa:webupd8team/java
+apt-get -y update
+apt-get -y install awscli
+apt-get -y install ruby2.0
+
+apt-get -y install supervisor
+
+cd /home/ubuntu
+aws s3 cp s3://aws-codedeploy-us-west-2/latest/install . --region us-west-2
+chmod +x ./install
+./install auto
+
+apt-get install oracle-java8-installer
+
+echo "JAVA_HOME='/usr/lib/jvm/java-8-oracle/jre/'" >> /etc/environment
+service supervisor start
