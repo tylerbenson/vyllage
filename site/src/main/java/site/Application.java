597831e1d40d8dab8b89d0a7a43b199dcc0a8636
Tyler Benson <tylerbenson@gmail.com>
2015-04-10 13:52:46 -0700

    Upgrade the spring boot version to 1.2.3.

--- site/src/main/java/site/Application.java
+++ site/src/main/java/site/Application.java
@@ -1,19 +1,17 @@
 package site;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
-@Configuration
+@SpringBootApplication
 @ComponentScan(basePackageClasses = { connections.Application.class,
 		documents.Application.class, accounts.Application.class,
 		Application.class })
 @PropertySource("classpath:/connections/application.properties")
 @PropertySource("classpath:/documents/application.properties")
 @PropertySource("classpath:/accounts/application.properties")
-@EnableAutoConfiguration
 public class Application {
 
 	public static void main(String[] args) {
