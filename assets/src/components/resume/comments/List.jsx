9cea60f81ff5b5647b1a02b7b88657fbae047fc4
kiran <kiran@webrafter.com>
2015-04-12 00:55:10 -0600

    Fix for timezone issue

--- assets/src/components/resume/comments/List.jsx
+++ assets/src/components/resume/comments/List.jsx
@@ -16,7 +16,7 @@ var CommentList = React.createClass({
               <div className='content'>
                 <div className='info'>
                   <div className="author">{comment.userName}</div>
-                  <div className="timestamp">{moment(comment.lastModified).tz(this.timezoneName).fromNow()}</div>
+                  <div className="timestamp">{moment.utc(comment.lastModified).fromNow()}</div>
                 </div>  
                 <div className="message">
                   {comment.commentText}
