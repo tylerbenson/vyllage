c9f6a45055200340a695aece7662f47eefdeb5a9
Tyler Benson <tylerbenson@gmail.com>
2015-04-13 23:43:55 -0700

    Switch back to in memory db
    At least for now.

--- scripts/deploy/before-install.sh
+++ scripts/deploy/before-install.sh
@@ -8,6 +8,7 @@ sudo mkdir -p /opt/vyllage/log/
 sudo chmod -R 777 /opt/vyllage
 
 /usr/bin/supervisorctl reread
+/usr/bin/supervisorctl update
 
 rm /opt/vyllage/*.jar || true
 rm /opt/vyllage/appspec.yml || true
