01e1a345e651a73ed66f8e731221cac486600972
kiran <kiran@webrafter.com>
2015-04-15 14:15:34 +0530

    Fixed #348

--- assets/src/components/resume/freeform/index.jsx
+++ assets/src/components/resume/freeform/index.jsx
@@ -1,6 +1,7 @@
 var React = require('react');
 var actions = require('../actions');
 var EditBtn = require('../../buttons/edit');
+var AddBtn = require('../../buttons/add');
 var DeleteSection = require('../Delete');
 var SaveBtn = require('../../buttons/save');
 var CancelBtn = require('../../buttons/cancel');
@@ -11,7 +12,6 @@ var Freeform = React.createClass({
   getInitialState: function() {
     return {
       description: this.props.section.description,
-      uiEditMode: false
     };
   },
   getDefaultProps: function () {
@@ -25,8 +25,10 @@ var Freeform = React.createClass({
       description: nextProps.section.description,
     });
   },
-  componentDidMount: function () {
-    this.refs.description.getDOMNode().focus();
+  componentDidUpdate: function() {
+    if (this.state.uiEditMode) {
+      this.refs.organizationName.getDOMNode().focus();
+    }
   },
   handleChange: function(e) {
     e.preventDefault();
@@ -53,8 +55,17 @@ var Freeform = React.createClass({
       this.refs.description.getDOMNode().focus();
     });
   },
+  addSection: function (e) {
+    actions.postSection({
+      title: this.props.title.toLowerCase(),
+    });
+    this.setState({
+      uiEditMode: true
+    })
+  },
   render: function () {
     var uiEditMode = this.state.uiEditMode;
+    var AddOrEditButton = this.props.section.sectionId ? <EditBtn onClick={this.editHandler}/> : <AddBtn onClick={this.addSection} />
     return (
       <div className='section'>
         <div className='header'>
@@ -62,23 +73,25 @@ var Freeform = React.createClass({
             <h1>{this.props.title}</h1>
           </div>
           <div className="actions">
-            {uiEditMode? <SaveBtn onClick={this.saveHandler}/>: <EditBtn onClick={this.editHandler}/>}
+            {uiEditMode? <SaveBtn onClick={this.saveHandler}/>: AddOrEditButton }
             {uiEditMode? <CancelBtn onClick={this.cancelHandler}/>: ''}
           </div>
         </div>
-        <div className="content">
-          <Textarea
-            ref='description'
-            disabled={!uiEditMode}
-            className="flat"
-            rows="1"
-            autoComplete="off"
-            placeholder="Tell us more.."
-            value={this.state.description}
-            onChange={this.handleChange}
-          ></Textarea>
-        </div>
-        <SectionFooter section={this.props.section} />
+        {this.props.section.sectionId ? <div>
+          <div className="content">
+            <Textarea
+              ref='description'
+              disabled={!uiEditMode}
+              className="flat"
+              rows="1"
+              autoComplete="off"
+              placeholder="Tell us more.."
+              value={this.state.description}
+              onChange={this.handleChange}
+            ></Textarea>
+          </div>
+          <SectionFooter section={this.props.section} />
+        </div>: <p className='content empty'>No {this.props.title.toLowerCase()} added yet</p> }
       </div>
     );
   }
