3ae646334e0e6c8eff3c3e5cbc2e021b2b41c902
Tyler Benson <tylerbenson@gmail.com>
2015-04-20 00:29:49 -0700

    Proper account data for prod.
    Current state is pretty screwed up.  Needs more work.

--- accounts/src/main/resources/accounts/db/pg/migration/V1__init_accounts.sql
+++ accounts/src/main/resources/accounts/db/pg/migration/V1__init_accounts.sql
@@ -30,33 +30,14 @@ create unique index ix_username_password on accounts.user_credentials (user_id, 
 create table if not exists accounts.roles(
 	role varchar(50) primary key);
 
-create table if not exists accounts.user_roles (
-  user_id bigint not null,
-  role varchar(50) not null,
-  constraint fk_roles_users foreign key(user_id) references accounts.users(user_id),
-  constraint fk_user_roles_roles foreign key(role) references accounts.roles(role));
-
-create unique index ix_auth_username on accounts.user_roles (user_id, role);
-
 create table if not exists accounts.organizations (
   organization_id bigserial primary key,
   organization_name varchar(50) not null);
 
-create table if not exists accounts.organization_roles (
+create table if not exists accounts.user_organization_roles (
   organization_id bigint not null,
-  role varchar(50) not null,
-  constraint fk_organization_roles_organization foreign key(organization_id) references accounts.organizations(organization_id));
-
-create table if not exists accounts.organization_members (
-  id bigserial primary key,
   user_id bigint not null,
-  organization_id bigint not null,
-  constraint fk_organization_members_organization foreign key(organization_id) references accounts.organizations(organization_id),
-  constraint fk_organization_members_users foreign key(user_id) references accounts.users(user_id));
-
-create table if not exists accounts.persistent_logins (
-  user_name varchar(64) not null,
-  series varchar(64) primary key,
-  token varchar(64) not null,
-  last_used timestamp not null);
-  
+  role varchar(50) not null,
+  constraint fk_user_organization_roles_organization foreign key(organization_id) references accounts.organizations(organization_id),
+  constraint fk_user_organization_roles_users foreign key(user_id) references accounts.users(user_id),
+  constraint fk_user_organization_roles_role foreign key(role) references accounts.roles(role));
