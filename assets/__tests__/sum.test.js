05c8e11640262dad915b6b00472b86e6a4cfcb37
kiran <kiran@webrafter.com>
2015-04-21 23:17:09 +0530

    Change to test if circleci reports error if test fails

--- assets/__tests__/sum.test.js
+++ assets/__tests__/sum.test.js
@@ -3,6 +3,6 @@ var sum = require('../src/sum');
 
 describe('sum', function() {
   it('adds 1 + 2 to equal 3', function() {
-    expect(sum(1, 2)).toBe(3);
+    expect(sum(1, 2)).toBe(4);
   });
 });
