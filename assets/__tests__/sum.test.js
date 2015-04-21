81a34896e075442add069939ed9599ecf2a96d8e
kiran <kiran@webrafter.com>
2015-04-21 22:29:39 +0530

    Basic testing setup

---
+++ assets/__tests__/sum.test.js
@@ -0,0 +1,8 @@
+// jest.dontMock('../src/sum');
+var sum = require('../src/sum');
+
+describe('sum', function() {
+  it('adds 1 + 2 to equal 3', function() {
+    expect(sum(1, 2)).toBe(3);
+  });
+});
