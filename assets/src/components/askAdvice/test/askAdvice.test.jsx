7c18d19a4bd01ff698b77f8fe887aef304d0db65
kiran <kiran@webrafter.com>
2015-04-29 21:42:11 +0530

    Basic test for ask advice

---
+++ assets/src/components/askAdvice/test/askAdvice.test.jsx
@@ -0,0 +1,10 @@
+var AskAdvice = require('../index');
+var React = require('react/addons');
+var TestUtils = React.addons.TestUtils;
+
+describe('Editor', function() {
+  it('Should render properly', function() {
+    var askAdvice = TestUtils.renderIntoDocument(<AskAdvice />)
+    expect(TestUtils.isCompositeComponent(askAdvice)).toBe(true);
+  });
+});
