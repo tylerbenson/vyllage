1c3ecf1ffbcd8d7444c85c5a9c657a8a586c8783
uh <carlos.uh@gmail.com>
2015-04-29 22:29:04 -0300

    Working console.

--- accounts/src/main/java/accounts/config/BeansConfiguration.java
+++ accounts/src/main/java/accounts/config/BeansConfiguration.java
@@ -1,10 +1,12 @@
 package accounts.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.web.client.RestTemplate;
+import org.togglz.console.TogglzConsoleServlet;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -37,6 +39,12 @@ public class BeansConfiguration {
 		EmailBuilder emailBuilder = new EmailBuilder(mailService(), environment);
 
 		return emailBuilder;
+	}
+
+	@Bean
+	public ServletRegistrationBean servletRegistrationBean() {
+		return new ServletRegistrationBean(new TogglzConsoleServlet(),
+				"/togglz/*");
 	}
 
 	// @Bean
