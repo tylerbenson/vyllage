01587828fc5d8fcdbb1edc8ff16289b760390614
kiran <kiran@webrafter.com>
2015-04-30 20:30:20 +0530

    Fixed failing resume editor tests

--- assets/src/components/resume/test/editor.test.jsx
+++ assets/src/components/resume/test/editor.test.jsx
@@ -3,6 +3,13 @@ var React = require('react/addons');
 var TestUtils = React.addons.TestUtils;
 
 describe('Editor', function() {
+
+  beforeEach(function () {
+    this.server = sinon.fakeServer.create();
+    this.server.respondWith("GET", "/resume//header.json", [200, { "Content-Type": "application/json" }, '']);
+    this.server.respondWith("GET", "/resume//section.json", [200, { "Content-Type": "application/json" }, '']);
+  });
+  
   it('Should render properly', function() {
     var resumeEditor = TestUtils.renderIntoDocument(<ResumeEditor />)
     expect(TestUtils.isCompositeComponent(resumeEditor)).toBe(true);
@@ -14,7 +21,5 @@ describe('Editor', function() {
     var nodes = TestUtils.scryRenderedDOMComponentsWithClass(resumeEditor, 'subheader');
     expect(nodes.length).toBe(0);
   });
-
-  
   
 });
