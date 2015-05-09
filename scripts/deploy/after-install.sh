7d540d8af458e0ae4f517f5b8d5657dfcb91055d
Tyler Benson <tyler@newrelic.com>
2015-05-08 23:47:40 -0700

    Reset folder each deploy

--- scripts/deploy/after-install.sh
+++ scripts/deploy/after-install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+sudo chmod 777 /opt/vyllage/newrelic
 ln -s /opt/vyllage/site-*.jar /opt/vyllage/site.jar
 ln -s /opt/vyllage/newrelic/newrelic-agent-*.jar /opt/vyllage/newrelic/newrelic.jar
--- scripts/deploy/before-install.sh
+++ scripts/deploy/before-install.sh
@@ -4,13 +4,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd -P "$( dirname "$SOURCE" )" 
 sudo cp $DIR/../supervisord.conf /etc/supervisor/
 sudo cp $DIR/../vyllage.conf /etc/supervisor/conf.d/
 
+rm -rf /opt/vyllage/ || true
 sudo mkdir -p /opt/vyllage/log/
 sudo chmod -R 777 /opt/vyllage
-sudo chmod -R 777 /opt/vyllage/newrelic
 
 /usr/bin/supervisorctl reread
 /usr/bin/supervisorctl update
-
-rm /opt/vyllage/*.jar || true
-rm /opt/vyllage/appspec.yml || true
-rm -r /opt/vyllage/scripts || true
