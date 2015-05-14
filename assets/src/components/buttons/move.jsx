5f5e484cb795e43fae83de4c893603cc7eec1ef7
kiran <kiran@webrafter.com>
2015-05-14 12:58:04 +0530

    Fixed #504

--- assets/src/components/buttons/move.jsx
+++ assets/src/components/buttons/move.jsx
@@ -3,10 +3,10 @@ var React = require('react');
 var MoveBtn = React.createClass({
   render: function() {
     return (
-      <button className="inverted secondary small move" {...this.props}>
+      <a style={{textAlign: 'center'}} className="inverted secondary button small move" {...this.props}>
         <i className="ion-arrow-move"></i>
         Move
-      </button>
+      </a>
     );
   }
 });
