bac3e52ed84fffa07f0989870b46c64463b4e8de
uh <carlos.uh@gmail.com>
2015-05-13 23:17:06 -0300

    Backend compression.

--- accounts/src/main/java/accounts/config/BeansConfiguration.java
+++ accounts/src/main/java/accounts/config/BeansConfiguration.java
@@ -2,12 +2,19 @@ package accounts.config;
 
 import javax.sql.DataSource;
 
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.AbstractHttp11Protocol;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.http.MediaType;
 import org.springframework.web.client.RestTemplate;
 import org.thymeleaf.spring4.SpringTemplateEngine;
 import org.togglz.console.TogglzConsoleServlet;
@@ -89,6 +96,36 @@ public class BeansConfiguration {
 		ApplicationContextProvider provider = new ApplicationContextProvider();
 		provider.setApplicationContext(applicationContext);
 		return provider;
+	}
+
+	@Bean
+	public EmbeddedServletContainerCustomizer servletContainerCustomizer() {
+		return new EmbeddedServletContainerCustomizer() {
+			@Override
+			public void customize(
+					ConfigurableEmbeddedServletContainer servletContainer) {
+				((TomcatEmbeddedServletContainerFactory) servletContainer)
+						.addConnectorCustomizers(new TomcatConnectorCustomizer() {
+							@Override
+							public void customize(Connector connector) {
+								AbstractHttp11Protocol httpProtocol = (AbstractHttp11Protocol) connector
+										.getProtocolHandler();
+
+								httpProtocol.setCompression("on");
+								httpProtocol.setCompressionMinSize(256);
+
+								String mimeTypes = httpProtocol
+										.getCompressableMimeTypes();
+
+								String mimeTypesWithJson = mimeTypes + ","
+										+ MediaType.APPLICATION_JSON_VALUE;
+
+								httpProtocol
+										.setCompressableMimeTypes(mimeTypesWithJson);
+							}
+						});
+			}
+		};
 	}
 
 }
