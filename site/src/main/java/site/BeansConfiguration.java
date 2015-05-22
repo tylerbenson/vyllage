ed209db037f1fa03c61500d612aba6f2391f5c64
Tyler Benson <tyler@newrelic.com>
2015-05-21 21:47:06 -0700

    Profiles cleanup.

--- site/src/main/java/site/BeansConfiguration.java
+++ site/src/main/java/site/BeansConfiguration.java
@@ -4,11 +4,12 @@ import org.h2.server.web.WebServlet;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration(value = "site.BeansConfiguration")
 public class BeansConfiguration {
 	@Bean
-	// @Profile(Profiles.STANDALONE)
+	@Profile(Profiles.DEV)
 	public ServletRegistrationBean h2Console() {
 		ServletRegistrationBean reg = new ServletRegistrationBean(
 				new WebServlet(), "/db-console/*");
