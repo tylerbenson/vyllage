cd7d57651407e3a8c2aa6947719acf1cfcfce690
Tyler Benson <tyler@newrelic.com>
2015-05-29 01:00:28 -0700

    Disable facebook by default and sort alphabetically.

--- togglz/src/main/java/togglz/Features.java
+++ togglz/src/main/java/togglz/Features.java
@@ -7,27 +7,27 @@ import org.togglz.core.context.FeatureContext;
 
 public enum Features implements Feature {
 
+	@Label("Facebook SDK")
+	FACEBOOK_SDK,
+
 	@EnabledByDefault
 	@Label("Google Analytics")
 	GOOGLE_ANALYTICS,
 
 	@EnabledByDefault
+	@Label("Intercom")
+	INTERCOM,
+
+	@EnabledByDefault
 	@Label("New Relic")
 	NEW_RELIC,
-
-	@EnabledByDefault
-	@Label("Zopim Message Client")
-	ZOPIM_MESSAGE_CLIENT,
-
-	@EnabledByDefault
-	@Label("Facebook SDK")
-	FACEBOOK_SDK,
 
 	@Label("Share Resume")
 	SHARE_RESUME,
 
-	@Label("Intercom")
-	INTERCOM;
+	@EnabledByDefault
+	@Label("Zopim Message Client")
+	ZOPIM_MESSAGE_CLIENT;
 
 	public boolean isActive() {
 		return FeatureContext.getFeatureManager().isActive(this);
