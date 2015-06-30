c9418c2b3f4e43cf71c87ad8e566f73a33110dc1
Aaron Noel De Leon <aaronnoeldeleon@gmail.com>
2015-06-30 20:17:04 +0800

    Added mock server response on tests

--- assets/src/components/getFeedback/test/getFeedback.test.jsx
+++ assets/src/components/getFeedback/test/getFeedback.test.jsx
@@ -3,6 +3,11 @@ var React = require('react/addons');
 var TestUtils = React.addons.TestUtils;
 
 describe('GetFeedback', function() {
+	beforeEach(function () {
+    this.server = sinon.fakeServer.create();
+    this.server.respondWith("GET", "/togglz-feature//SUGGESTIONS//is-active.json", [200, { "Content-Type": "application/json" }, 'true']);
+  });
+
   it('Should render properly', function() {
     var getFeedback = TestUtils.renderIntoDocument(<GetFeedback />)
     expect(TestUtils.isCompositeComponent(getFeedback)).toBe(true);
