f50facb1957a445f1fa97ca9953f5969213809fd
uh <carlos.uh@gmail.com>
2015-07-01 11:44:55 -0300

    Added ADVISOR role back.

--- accounts/src/main/resources/accounts/db/pg/migration/V7__new_roles_suggestion.sql
+++ accounts/src/main/resources/accounts/db/pg/migration/V7__new_roles_suggestion.sql
@@ -1,7 +1,7 @@
-alter table ACCOUNTS.user_organization_roles drop constraint fk_organization_roles_role; 
+alter table ACCOUNTS.user_organization_roles drop constraint fk_user_organization_roles_role; 
 update ACCOUNTS.roles set role = 'ACADEMIC_ADVISOR' where role = 'ADVISOR';
 update ACCOUNTS.user_organization_roles set role = 'ACADEMIC_ADVISOR' where role = 'ADVISOR';
-alter table ACCOUNTS.user_organization_roles add constraint fk_organization_roles_role foreign key(role) references ACCOUNTS.roles(role);
+alter table ACCOUNTS.user_organization_roles add constraint fk_user_organization_roles_role foreign key(role) references ACCOUNTS.roles(role);
 
 insert into ACCOUNTS.roles ( role ) values ( 'ADMISSIONS_ADVISOR' );
 insert into ACCOUNTS.roles ( role ) values ( 'CAREER_ADVISOR' );
