36250c11748e670d8fc5b99370946f8bff7e3ff1
uh <carlos.uh@gmail.com>
2015-07-10 14:02:34 -0300

    Removed test user.

--- accounts/src/main/resources/accounts/db/pg/migration/V2__data_accounts.sql
+++ accounts/src/main/resources/accounts/db/pg/migration/V2__data_accounts.sql
@@ -7,15 +7,12 @@ insert into accounts.roles ( role ) values ( 'STUDENT' );
 
 insert into accounts.users (user_id, user_name, first_name, middle_name, last_name, enabled, date_created, last_modified) values (0, 'nathan@vyllage.com', 'Nathan', 'M', 'Benson', true, current_date, current_date);
 insert into accounts.users (user_id, user_name, first_name, middle_name, last_name, enabled, date_created, last_modified) values (1, 'tyler@vyllage.com', 'Tyler', 'D', 'Benson', true, current_date, current_date);
-insert into accounts.users (user_id, user_name, first_Name, middle_name, last_name, enabled, date_created, last_modified) values (2, 'user@vyllage.com', 'Luke', 'V', 'Skywalker', true, CURRENT_DATE, CURRENT_DATE);
 
 insert into accounts.user_credentials (user_id, password, enabled, expires) values (0, '$2a$10$AD3tneImR8CkAiESv.zDruYMGjm5rNnmQEW22VmiRq8wgjlt1WfNm', true, null);
 insert into accounts.user_credentials (user_id, password, enabled, expires) values (1, '$2a$10$AD3tneImR8CkAiESv.zDruYMGjm5rNnmQEW22VmiRq8wgjlt1WfNm', true, null);
-insert into accounts.user_credentials (user_id, password, enabled, expires) values (2, '$2a$10$6REiiQD8i4FTE4D/X3.chOnlgxghwWMGcngksAjyQun0njGDQznxq', true, null);
 
 insert into accounts.organizations ( organization_id, organization_name ) values ( 0, 'Vyllage' );
 insert into accounts.organizations ( organization_id, organization_name ) values ( 1, 'Test' );
 
 insert into accounts.user_organization_roles ( organization_id, user_id, role, date_created, audit_user_id ) values ( 0, 0, 'ADMIN', current_date, 0 );
 insert into accounts.user_organization_roles ( organization_id, user_id, role, date_created, audit_user_id ) values ( 0, 1, 'ADMIN', current_date, 0 );
-insert into accounts.user_organization_roles ( organization_id, user_id, role, date_created, audit_user_id ) values ( 0, 2, 'ADMIN', current_date, 0 );
