b16f79755568f06a36f1b0537fe37b45da6dd5db
Aaron Noel De Leon <aaronnoeldeleon@gmail.com>
2015-07-15 15:39:45 +0800

    Revert "Merge branch 'master' into nelonoel/boarding-fixes"
    This reverts commit f08477458b1a485bd983ef4624c6b72dc50c4498, reversing
    changes made to e2c896cb0392f32a0e72934e77f68bdc9b6771cb.

--- assets/src/components/resume/Print.jsx
+++ assets/src/components/resume/Print.jsx
@@ -1,6 +1,5 @@
 var React = require('react');
 var Reflux = require('reflux');
-var Modal = require('../modal');
 var actions = require('./actions');
 var resumeStore = require('./store');
 
@@ -9,45 +8,12 @@ var Print = React.createClass({
   componentWillMount: function () {
     actions.getResume();
   },
-  handlePrint: function(e) {
-    if(this.state.resume.sections.length < 1) {
-      e.preventDefault();
-      actions.openEmptyResumeModal();
-    }
-  },
-  closeModal: function(e) {
-    e.preventDefault();
-    actions.closeEmptyResumeModal();
-  },
 	render: function() {
 		return (
-      <span>
-  			<a onClick={this.handlePrint} href={"/resume/"+this.state.resume.documentId+"/file/pdf"} className="flat print button">
-          <i className="ion-printer"></i>
-          <span>Print</span>
-        </a>
-        <Modal isOpen={this.state.resume.isEmptyResumeModalOpen} close={this.closeModal}>
-          <div className="header">
-            <div className="title">
-              <h1>Resumé Empty</h1>
-            </div>
-            <div className="actions">
-              <button className="secondary flat icon" onClick={this.closeModal}>
-                <i className="ion-close"></i>
-              </button>
-            </div>
-          </div>
-          <div className="content">
-            <p>Please add sections before printing your resumé.</p>
-          </div>
-          <div className="footer">
-            <button onClick={this.closeModal} className="small inverted">
-              <i className="ion-checkmark"></i>
-              Okay
-            </button>
-          </div>
-        </Modal>
-      </span>
+			<a href={"/resume/"+this.state.resume.documentId+"/file/pdf"} className="flat print button">
+        <i className="ion-printer"></i>
+        <span>Print</span>
+      </a>
 		);
 	}
 
