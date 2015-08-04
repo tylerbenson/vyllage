4190ef135cfb1762d8c3637feb556d9f57282431
uh <carlos.uh@gmail.com>
2015-08-03 22:23:54 -0300

    Fixed wrong user id. Added Colorado Christian University.

--- accounts/src/main/resources/accounts/db/pg/migration/V10__navarro_university.sql
+++ accounts/src/main/resources/accounts/db/pg/migration/V10__navarro_university.sql
@@ -1,2 +1,4 @@
 insert into accounts.organizations ( organization_id, organization_name ) values ( 5, 'Navarro University' );
-insert into accounts.user_organization_roles ( organization_id, user_id, role, date_created, audit_user_id ) values ( 0, 1, 'ADVISOR', current_date, 0 );
+insert into accounts.user_organization_roles ( organization_id, user_id, role, date_created, audit_user_id ) values ( 0, 0, 'ADVISOR', current_date, 0 );
+insert into accounts.organizations ( organization_id, organization_name ) values ( 6, 'Colorado Christian University' );
+insert into accounts.user_organization_roles ( organization_id, user_id, role, date_created, audit_user_id ) values ( 0, 0, 'ADVISOR', current_date, 0 );
