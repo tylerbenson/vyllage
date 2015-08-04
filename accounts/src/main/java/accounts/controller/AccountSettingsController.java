be49934eb22780606893b3fe38e3629d51ebbd4f
uh <carlos.uh@gmail.com>
2015-08-04 11:43:54 -0300

    Checks if a user is connected to a given social network.

--- accounts/src/main/java/accounts/controller/AccountSettingsController.java
+++ accounts/src/main/java/accounts/controller/AccountSettingsController.java
@@ -16,6 +16,7 @@ import javax.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
+import org.springframework.social.connect.web.ProviderSignInUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -26,6 +27,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
 
 import user.common.User;
 import user.common.constants.RolesEnum;
@@ -68,6 +70,8 @@ public class AccountSettingsController {
 	private final AccountSettingsService accountSettingsService;
 	private final DocumentService documentService;
 	private final OrganizationRepository organizationRepository;
+
+	private ProviderSignInUtils providerSignInUtils = new ProviderSignInUtils();
 
 	private Map<String, SettingValidator> validators = new HashMap<>();
 	private List<SettingValidator> validatorsForAll = new LinkedList<>();
@@ -308,6 +312,15 @@ public class AccountSettingsController {
 					.getUserId().toString(), "")));
 
 		return permissions;
+	}
+
+	@RequestMapping(value = "social/{network}/is-connected", method = RequestMethod.GET, produces = "application/json")
+	public @ResponseBody boolean isSocialConnected(WebRequest webRequest,
+			@PathVariable String network, @AuthenticationPrincipal User user) {
+
+		// expireTime != null && System.currentTimeMillis() >= expireTime;
+
+		return false;
 	}
 
 	@ExceptionHandler(value = { IllegalArgumentException.class })
