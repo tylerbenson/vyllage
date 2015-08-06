f6438542ed8803d553e33afd8561d60d05449e5e
uh <carlos.uh@gmail.com>
2015-08-06 14:41:11 -0300

    remove comment

--- accounts/src/main/java/accounts/controller/ConnectControllerWithRedirect.java
+++ accounts/src/main/java/accounts/controller/ConnectControllerWithRedirect.java
@@ -19,16 +19,6 @@ public class ConnectControllerWithRedirect extends ConnectController {
 		this.setViewPath("/");
 	}
 
-	/**
-	 * Returns the view name of a page to display for a provider when the user
-	 * is connected to the provider. Typically this page would allow the user to
-	 * disconnect from the provider. Defaults to
-	 * "connect/{providerId}Connected". May be overridden to return a custom
-	 * view name.
-	 * 
-	 * @param providerId
-	 *            the ID of the provider to display the connection status for.
-	 */
 	@Override
 	protected String connectedView(String providerId) {
 		return "redirect:/account/setting";
