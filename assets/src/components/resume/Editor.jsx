5570db4af800e80fbb5d6f377394ba2b40c404de
tarex <tarexyz@gmail.com>
2015-08-11 22:23:20 +0600

    basic sorting

--- assets/src/components/resume/Editor.jsx
+++ assets/src/components/resume/Editor.jsx
@@ -9,6 +9,28 @@ var Section = require('./sections');
 var Banner = require('./banner');
 var sortby = require('lodash.sortby');
 var EmptySections = require('./sections/Empty');
+var Reorderable = require('./../reorderable');
+
+  
+
+  
+  var ListItem =  React.createClass({   
+      render: function () {
+      
+        return (
+          <div> 
+            <span className="moveme">MOVE</span>
+              {this.props.item.title}
+
+          </div>
+        )
+
+      }
+  });
+  
+
+
+
 
 var ResumeEditor = React.createClass({
   mixins: [Reflux.connect(resumeStore, 'resume'), Reflux.connect(settingStore)],
@@ -76,11 +98,47 @@ var ResumeEditor = React.createClass({
 
     return sectionGroupNodes;
   },
+
+
+
+
   render: function () {
     var owner=this.state.resume.header.owner;
+  
+   
+
     return (
       <div>
         <Banner header={this.state.resume.header} settings={this.state.settings} />
+
+        <Reorderable
+         
+          itemKey='sectionId'
+         
+          lock='horizontal'
+         
+          holdTime='0'
+
+          handle="moveme"
+         
+          list={this.state.resume.sections}
+         
+          template={ListItem}
+         
+          callback={this.callback}
+        
+          listClass='my-list'
+        
+          itemClass='list-item'
+         
+          selected={this.state.selected}
+        
+          selectedKey='sectionId'
+       
+          disableReorder={false}/>
+
+              
+
         <div className="sections">
           {this.renderSections()}
           {owner ? <EmptySections sections={this.state.resume.sections} owner={owner} />: null}
