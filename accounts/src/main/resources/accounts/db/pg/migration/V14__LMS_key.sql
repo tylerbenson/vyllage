0972b28fabbb9c9067c9b21e881dd3e80c761743
uh <carlos.uh@gmail.com>
2015-08-12 14:15:56 -0300

    Added type.

--- accounts/src/main/resources/accounts/db/pg/migration/V14__LMS_key.sql
+++ accounts/src/main/resources/accounts/db/pg/migration/V14__LMS_key.sql
@@ -2,7 +2,7 @@ create table if not exists accounts.lms_key(
   key_id bigserial primary key,
   consumer_key varchar(256) not null,
   creator_user_id bigint not null,
-  modified_by_user_id not null,
+  modified_by_user_id bigint not null,
   date_created timestamp not null,
   last_modified timestamp not null,
   constraint fk_lms_key_creator_users foreign key(user_id) references accounts.users(user_id),
