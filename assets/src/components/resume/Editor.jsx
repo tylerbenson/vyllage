edfeaac383d6756e16441f1dea6484207314c6e5
Aaron Noel De Leon <aaronnoeldeleon@gmail.com>
2015-08-20 17:42:45 +0800

    Created Sortable component

--- assets/src/components/resume/Editor.jsx
+++ assets/src/components/resume/Editor.jsx
@@ -9,115 +9,97 @@ var Section = require('./sections');
 var Banner = require('./banner');
 var sortby = require('lodash.sortby');
 var Empty = require('./sections/Empty');
-
-window.jQuery = require('jquery');
-require('jquery-ui/sortable');
-require('jquery-ui-touch-punch');
+var Sortable = require('../util/Sortable');
 
 var SubSection = React.createClass({
+  start: function(event, ui){
+    jQuery('.banner .subheader').fadeOut(200);
+  },
+  stop: function(event, ui){
+    var subsection_order = [];
+    var type;
 
-    componentDidMount: function () {
-        var self = this;
+    jQuery(event.target).children().each(function(index) {
+      subsection_order.push({
+        sectionId : jQuery(this).data("id"),
+        index : index
+      });
 
-        jQuery('.subsection-holder').sortable({
-            axis : 'y',
-            placeholder: "ui-sortable-placeholder",
-            opacity: 0.85,
-            scrollSpeed: 15,
-            scrollSensitivity: 20,
-            forcePlaceholderSize: true,
-            cursor: "move",
-            items: "div.subsection-wrapper",
-            handle:'.move-sub',
-            start: function(event, ui) {
-              jQuery('.banner .subheader').hide();
-            },
-            stop : function(event, ui){
-              jQuery('.banner .subheader').show();
-              var subsection_order = [];
-              var type;
-              jQuery(this).children().each(function(index) {
-                  subsection_order.push({
-                    sectionId : jQuery(this).data('id'),
-                    index : index
-                  });
+      type = jQuery(this).attr("rel");
+    });
 
-                  type = jQuery(this).attr('rel');
-              });
-
-              actions.moveSectionOrder(subsection_order , type );
-            }
-        });
-    },
-
-    render: function(){
-      return(
-        <div className="subsection-wrapper" rel={this.props.data.type} data-id={this.props.data.sectionId}>
-          <Section
-            key={this.props.data.sectionId}
-            section={this.props.data}
-            owner={this.props.owner}
-           />
-         </div>
-      );
-    }
+    actions.moveSectionOrder(subsection_order , type);
+    jQuery('.banner .subheader').fadeIn(200);
+  },
+  render: function(){
+    var config = {
+      list: ".subsection-holder",
+      items: "div.subsection-wrapper",
+      handle: ".move-sub",
+      start: this.start,
+      stop: this.stop
+    };
+    return(
+      <Sortable config={config} className="subsection-wrapper" rel={this.props.data.type} data-id={this.props.data.sectionId}>
+        <Section
+          key={this.props.data.sectionId}
+          section={this.props.data}
+          owner={this.props.owner}
+         />
+       </Sortable>
+    );
+  }
 });
 
 var SectionGroup = React.createClass({
-    componentDidMount: function () {
-        var self = this;
+  start: function(event, ui) {
+    jQuery('.banner .subheader').fadeOut(200);
+  },
+  stop: function(event, ui){
+    var order = [];
 
-        jQuery('.section-holder').sortable({
-            axis : 'y',
-            placeholder: "ui-sortable-placeholder",
-            opacity: 0.85,
-            scrollSpeed: 15,
-            scrollSensitivity: 20,
-            forcePlaceholderSize: true,
-            cursor: "move",
-            items: "div.section",
-            handle: ".move-section",
-            start: function(event, ui) {
-              jQuery('.banner .subheader').hide();
-            },
-            stop : function(event, ui){
-              jQuery('.banner .subheader').show();
-              var order = [];
-              jQuery('.section-holder').children().each(function(index) {
-                  order.push({
-                    type : jQuery(this).attr('rel'),
-                    index : index
-                  });
-              });
-              actions.moveGroupOrder(order);
-            }
+    jQuery(event.target).children().each(function(index) {
+        order.push({
+          type : jQuery(this).attr("rel"),
+          index : index
         });
-    },
-    render: function(){
-      var self = this;
-      var subsection = function( sub_section , index ){
-          return <SubSection key={index} data={sub_section} owner={self.props.section.owner} />
-      }
-      return (
-        <div className="section" rel={this.props.section.type}>
-          <div className="container">
-            <span className="inverted secondary button small move move-section" {...this.props}>
-              <i className="ion-arrow-move"></i>
-              Move
-            </span>
-            <Header
-              title={this.props.section.title}
-              type={this.props.section.type}
-              owner={this.props.section.owner}
-            />
+    });
 
-            <div className="subsection-holder">
-              { this.props.section.child.map(subsection) }
-            </div>
+    actions.moveGroupOrder(order);
+    jQuery('.banner .subheader').fadeIn(200);
+  },
+  render: function(){
+    var self = this;
+    var subsection = function( sub_section , index ){
+        return <SubSection key={index} data={sub_section} owner={self.props.section.owner} />
+    };
+    var config = {
+      list: ".section-holder",
+      items: "div.section",
+      handle: ".move-section",
+      start: this.start,
+      stop: this.stop
+    };
+    return (
+      <Sortable config={config} className="section" rel={this.props.section.type}>
+        <div className="container">
+          <span className="inverted secondary button small move move-section" {...this.props}>
+            <i className="ion-arrow-move"></i>
+            Move
+          </span>
+          <Header
+            title={this.props.section.title}
+            type={this.props.section.type}
+            owner={this.props.section.owner}
+          />
+
+          <div className="subsection-holder">
+            { this.props.section.child.map(subsection) }
           </div>
         </div>
-      )
-    }
+      </Sortable>
+    )
+  }
 });
 
 
