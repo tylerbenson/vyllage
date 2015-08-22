863e62c17f20cb1d8f3da302ff279b1aa03f9aa4
uh <carlos.uh@gmail.com>
2015-08-22 03:08:03 -0300

    Removed schema from index creation for PostgreSQL.

--- accounts/src/main/resources/accounts/db/pg/migration/V16__organization_external_id.sql
+++ accounts/src/main/resources/accounts/db/pg/migration/V16__organization_external_id.sql
@@ -2,4 +2,4 @@ alter table accounts.lti_credentials add column external_organization_id varchar
 
 alter table accounts.lti_credentials add constraint uq_external_organization_id unique (external_organization_id);
 
-create unique index accounts.ix_organization_external_organization on accounts.lti_credentials (key_id, external_organization_id);
+create unique index ix_organization_external_organization on accounts.lti_credentials (key_id, external_organization_id);
