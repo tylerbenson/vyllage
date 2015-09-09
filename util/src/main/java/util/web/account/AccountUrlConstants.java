87e4ace922a674889c51921aeb582ae8ef8df9b6
uh <carlos.uh@gmail.com>
2015-09-08 22:16:30 -0300

    Changed page text. Added new constant for urls. Will create a different project later on another PR.

---
+++ util/src/main/java/util/web/account/AccountUrlConstants.java
@@ -0,0 +1,7 @@
+package util.web.account;
+
+public interface AccountUrlConstants {
+	public final String NEEDS_EMAIL_CONFIRMATION_VALID_PHONE_NUMBER = "needs-email-confirmation-valid-phone-number";
+
+	public final String EMAIL_CONFIRMATION_SUCCESS = "email-confirmation-success";
+}
