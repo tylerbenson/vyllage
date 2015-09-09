2da7b3f1d1670b419219eacbb140871e5d5e7c50
uh <carlos.uh@gmail.com>
2015-09-08 21:55:19 -0300

    Added validation for phonebumber presence for requesting feedback. Updated tests. Removed unused annotations.

--- accounts/src/test/java/accounts/controller/AccountControllerTest.java
+++ accounts/src/test/java/accounts/controller/AccountControllerTest.java
@@ -1,21 +1,30 @@
 package accounts.controller;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Optional;
+
 import org.apache.commons.codec.digest.DigestUtils;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.core.env.Environment;
 import org.springframework.security.crypto.encrypt.TextEncryptor;
 
 import user.common.User;
-import accounts.controller.AccountController;
 import accounts.mocks.SelfReturningAnswer;
+import accounts.model.Email;
+import accounts.model.account.settings.AccountSetting;
+import accounts.model.account.settings.Privacy;
 import accounts.repository.ElementNotFoundException;
 import accounts.repository.EmailRepository;
 import accounts.repository.UserNotFoundException;
+import accounts.service.AccountSettingsService;
 import accounts.service.DocumentLinkService;
 import accounts.service.UserService;
 import accounts.service.contactSuggestion.UserContactSuggestionService;
@@ -26,7 +35,7 @@ import email.EmailBuilder;
 
 public class AccountControllerTest {
 
-	private AccountController contoller;
+	private AccountController controller;
 
 	private Environment environment = mock(Environment.class);
 
@@ -35,6 +44,8 @@ public class AccountControllerTest {
 	private DocumentLinkService documentLinkService = mock(DocumentLinkService.class);
 
 	private UserContactSuggestionService userContactSuggestionService = mock(UserContactSuggestionService.class);
+
+	private AccountSettingsService accountSettingsService = mock(AccountSettingsService.class);
 
 	private EmailRepository emailRepository = mock(EmailRepository.class);
 
@@ -45,12 +56,15 @@ public class AccountControllerTest {
 
 	private TextEncryptor encryptor = mock(TextEncryptor.class);
 
+	private Object mockMvc;
+
 	@Before
 	public void setUp() {
 
-		contoller = new AccountController(environment, userService,
+		controller = new AccountController(environment, userService,
 				documentLinkService, userContactSuggestionService,
-				emailRepository, emailBuilder, encryptor, mapper);
+				accountSettingsService, emailRepository, emailBuilder,
+				encryptor, mapper);
 	}
 
 	@Test
@@ -61,10 +75,10 @@ public class AccountControllerTest {
 
 		when(userService.getAvatar(userId)).thenReturn(gravatarUrl);
 
-		String avatarUrl = contoller.getAvatar(userId);
+		String avatarUrl = controller.getAvatar(userId);
 
-		Assert.assertNotNull(avatarUrl);
-		Assert.assertTrue(gravatarUrl.contains(avatarUrl));
+		assertNotNull(avatarUrl);
+		assertTrue(gravatarUrl.contains(avatarUrl));
 	}
 
 	@Test
@@ -79,10 +93,10 @@ public class AccountControllerTest {
 		when(userService.getAvatar(userId)).thenReturn(facebookUrl);
 		when(userService.getUser(userId)).thenReturn(user);
 
-		String avatarUrl = contoller.getAvatar(userId);
+		String avatarUrl = controller.getAvatar(userId);
 
-		Assert.assertNotNull(avatarUrl);
-		Assert.assertTrue(facebookUrl.contains(avatarUrl));
+		assertNotNull(avatarUrl);
+		assertTrue(facebookUrl.contains(avatarUrl));
 	}
 
 	@Test
@@ -98,10 +112,140 @@ public class AccountControllerTest {
 		when(userService.getAvatar(userId)).thenReturn(gravatarUrl);
 		when(userService.getUser(userId)).thenReturn(user);
 
-		String avatarUrl = contoller.getAvatar(userId);
+		String avatarUrl = controller.getAvatar(userId);
 
-		Assert.assertNotNull(avatarUrl);
-		Assert.assertTrue(gravatarUrl.contains(avatarUrl));
+		assertNotNull(avatarUrl);
+		assertTrue(gravatarUrl.contains(avatarUrl));
 	}
 
+	@Test
+	public void canRequestFeedback() {
+		User user = mock(User.class);
+		Long userId = 0L;
+		String settingName = "phoneNumber";
+
+		when(accountSettingsService.getAccountSetting(user, settingName))
+				.thenReturn(
+						Optional.of(new AccountSetting(null, userId,
+								settingName, "1234567890", Privacy.PRIVATE
+										.name())));
+
+		when(emailRepository.getByUserId(userId))
+				.thenReturn(
+						Arrays.asList(new Email(userId, "email@email.com",
+								true, true)));
+
+		Boolean canRequestFeedback = controller
+				.canRequestFeedback(userId, user);
+
+		assertTrue(canRequestFeedback);
+	}
+
+	@Test
+	public void cannotRequestFeedbackNoConfirmedEmail() {
+		User user = mock(User.class);
+		Long userId = 0L;
+		String settingName = "phoneNumber";
+		boolean confirmed = false;
+
+		when(accountSettingsService.getAccountSetting(user, settingName))
+				.thenReturn(
+						Optional.of(new AccountSetting(null, userId,
+								settingName, "1234567890", Privacy.PRIVATE
+										.name())));
+
+		when(emailRepository.getByUserId(userId)).thenReturn(
+				Arrays.asList(new Email(userId, "email@email.com", true,
+						confirmed)));
+
+		Boolean canRequestFeedback = controller
+				.canRequestFeedback(userId, user);
+
+		assertFalse(canRequestFeedback);
+	}
+
+	@Test
+	public void cannotRequestFeedbackNoEmails() {
+		User user = mock(User.class);
+		Long userId = 0L;
+		String settingName = "phoneNumber";
+
+		when(accountSettingsService.getAccountSetting(user, settingName))
+				.thenReturn(
+						Optional.of(new AccountSetting(null, userId,
+								settingName, "1234567890", Privacy.PRIVATE
+										.name())));
+
+		when(emailRepository.getByUserId(userId)).thenReturn(
+				Collections.emptyList());
+
+		Boolean canRequestFeedback = controller
+				.canRequestFeedback(userId, user);
+
+		assertFalse(canRequestFeedback);
+	}
+
+	@Test
+	public void cannotRequestFeedbackEmptyPhoneNumber() {
+		User user = mock(User.class);
+		Long userId = 0L;
+		String settingName = "phoneNumber";
+		boolean confirmed = true;
+
+		when(accountSettingsService.getAccountSetting(user, settingName))
+				.thenReturn(
+						Optional.of(new AccountSetting(null, userId,
+								settingName, " ", Privacy.PRIVATE.name())));
+
+		when(emailRepository.getByUserId(userId)).thenReturn(
+				Arrays.asList(new Email(userId, "email@email.com", true,
+						confirmed)));
+
+		Boolean canRequestFeedback = controller
+				.canRequestFeedback(userId, user);
+
+		assertFalse(canRequestFeedback);
+	}
+
+	@Test
+	public void cannotRequestFeedbackNullPhoneNumber() {
+		User user = mock(User.class);
+		Long userId = 0L;
+		String settingName = "phoneNumber";
+		boolean confirmed = true;
+
+		when(accountSettingsService.getAccountSetting(user, settingName))
+				.thenReturn(
+						Optional.of(new AccountSetting(null, userId,
+								settingName, null, Privacy.PRIVATE.name())));
+
+		when(emailRepository.getByUserId(userId)).thenReturn(
+				Arrays.asList(new Email(userId, "email@email.com", true,
+						confirmed)));
+
+		Boolean canRequestFeedback = controller
+				.canRequestFeedback(userId, user);
+
+		assertFalse(canRequestFeedback);
+	}
+
+	@Test
+	public void cannotRequestFeedbackPhoneNumberNotPresent() {
+		User user = mock(User.class);
+		Long userId = 0L;
+		String settingName = "phoneNumber";
+		boolean confirmed = true;
+
+		when(accountSettingsService.getAccountSetting(user, settingName))
+				.thenReturn(Optional.empty());
+
+		when(emailRepository.getByUserId(userId)).thenReturn(
+				Arrays.asList(new Email(userId, "email@email.com", true,
+						confirmed)));
+
+		Boolean canRequestFeedback = controller
+				.canRequestFeedback(userId, user);
+
+		assertFalse(canRequestFeedback);
+	}
 }
