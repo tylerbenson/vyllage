e803977c4635d0b4db07a40bacbc5befc22fe1dd
uh <carlos.uh@gmail.com>
2015-09-18 15:39:45 -0300

    Grouped the api.

--- documents/src/main/java/documents/SwaggerConfig.java
+++ documents/src/main/java/documents/SwaggerConfig.java
@@ -1,25 +1,94 @@
 package documents;
 
+import static com.google.common.base.Predicates.or;
 import static springfox.documentation.builders.PathSelectors.regex;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger.web.UiConfiguration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import com.google.common.base.Predicate;
+
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+	// @Bean
+	// public Docket api() {
+	//
+	// AuthorizationScope[] authScopes = new AuthorizationScope[1];
+	// authScopes[0] = new AuthorizationScopeBuilder().scope("read")
+	// .description("read access").build();
+	//
+	// SecurityReference securityReference = SecurityReference.builder()
+	// .reference("test").scopes(authScopes).build();
+	//
+	// List<SecurityContext> securityContexts = newArrayList(SecurityContext
+	// .builder().securityReferences(newArrayList(securityReference))
+	// .build());
+	//
+	// return new Docket(DocumentationType.SWAGGER_2)
+	// .securitySchemes(newArrayList((new BasicAuth("test"))))
+	// .securityContexts(securityContexts).select()
+	// .apis(RequestHandlerSelectors.any()).paths( and(regex("/.*"), not()))
+	// .build().apiInfo(apiInfo());
+	// }
+
 	@Bean
-	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.any()).paths(regex("/.*"))
-				.build().apiInfo(apiInfo());
+	public Docket documentsApi() {
+		return new Docket(DocumentationType.SWAGGER_2).groupName("documents")
+				.select().paths(documentsPaths()).build().apiInfo(apiInfo());
+	}
+
+	protected Predicate<String> documentsPaths() {
+		return or(regex("/resume/.*"), regex("/document/.*"));
+	}
+
+	@Bean
+	public Docket accountsApi() {
+		return new Docket(DocumentationType.SWAGGER_2).groupName("accounts")
+				.select().paths(accountsPaths()).build().apiInfo(apiInfo());
+	}
+
+	@SuppressWarnings("unchecked")
+	protected Predicate<String> accountsPaths() {
+		return or(regex("/admin/.*"), regex("/account/.*"),
+				regex("/register/.*"));
+	}
+
+	@Bean
+	public Docket socialApi() {
+		return new Docket(DocumentationType.SWAGGER_2).groupName("social")
+				.select().paths(socialPaths()).build().apiInfo(apiInfo());
+	}
+
+	protected Predicate<String> socialPaths() {
+		return or(regex("/connect/.*"), regex("/disconnect/.*"));
+	}
+
+	@Bean
+	public Docket ltiApi() {
+		return new Docket(DocumentationType.SWAGGER_2).groupName("lti")
+				.select().paths(ltiPaths()).build().apiInfo(apiInfo());
+	}
+
+	protected Predicate<String> ltiPaths() {
+		return regex("/lti/.*");
+	}
+
+	@Bean
+	public Docket allApi() {
+		return new Docket(DocumentationType.SWAGGER_2).groupName("all")
+				.select().paths(allPaths()).build().apiInfo(apiInfo());
+	}
+
+	@SuppressWarnings("unchecked")
+	protected Predicate<String> allPaths() {
+		return regex("/.*");
 	}
 
 	private ApiInfo apiInfo() {
