914863ccd237c2329d79440a8200692943ce3e10
Tyler Benson <tylerbenson@gmail.com>
2015-09-20 23:19:12 -0700

    Remove text from RSS so module only displays button.

--- accounts/src/main/java/accounts/controller/LtiRssController.java
+++ accounts/src/main/java/accounts/controller/LtiRssController.java
@@ -1,7 +1,10 @@
 package accounts.controller;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.logging.Logger;
+
+import javax.servlet.http.HttpServletRequest;
+
+import oauth.lti.LMSRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,24 +12,36 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import accounts.model.rss.RssItem;
-
-@Controller()
+@Controller
 @RequestMapping("/lti")
 public class LtiRssController {
 
+	private final Logger logger = Logger.getLogger(LMSLoginController.class
+			.getName());
+
 	@RequestMapping(value = "rss", method = { RequestMethod.GET,
 			RequestMethod.POST }, produces = "application/rss+xml")
-	public ModelAndView getRss(Model model) {
-
+	public ModelAndView getRss(HttpServletRequest request, Model model) {
 		ModelAndView mav = new ModelAndView();
-
 		mav.setViewName("rssViewer");
-		List<RssItem> content = Arrays.asList(new RssItem(
-				"Improve your resume", "http://www.vyllage.com/resume/",
-				"For a better tomorrow"));
 
-		mav.addObject("feedContent", content);
+		if (request.getMethod().equals(RequestMethod.POST.name())) {
+			LMSRequest lmsRequest = null;
+			try {
+				lmsRequest = LMSRequest.getInstance();
+			} catch (IllegalStateException e) {
+				// Log but still return an "empty" document (no notifications).
+				logger.warning("Request did not validate as an LTI request.");
+			}
+			if (lmsRequest != null) {
+				// Remove RssItem's until we have notifications.
+				// List<RssItem> content = Arrays.asList(new RssItem(
+				// "Improve your resume", "http://www.vyllage.com/resume/",
+				// "For a better tomorrow"));
+				//
+				// mav.addObject("feedContent", content);
+			}
+		}
 
 		return mav;
 	}
