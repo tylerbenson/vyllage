72efcffaed56c66091c6472af63d4ced0868bdc6
uh <carlos.uh@gmail.com>
2015-09-21 11:44:00 -0300

    Fixed tests.

--- accounts/src/test/java/accounts/ApplicationTestConfig.java
+++ accounts/src/test/java/accounts/ApplicationTestConfig.java
@@ -2,7 +2,9 @@ package accounts;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.hateoas.HypermediaAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.FilterType;
@@ -13,6 +15,7 @@ import accounts.config.BeansConfiguration;
 @SpringBootApplication
 @ComponentScan(basePackages = { "accounts" }, excludeFilters = { @Filter(type = FilterType.ASSIGNABLE_TYPE, value = {
 		BeansConfiguration.class, Application.class }) })
+@EnableAutoConfiguration(exclude = { HypermediaAutoConfiguration.class })
 @PropertySource("classpath:/accounts/application.properties")
 public class ApplicationTestConfig implements CommandLineRunner {
 
