bb397143e0cfb28f05c03015bde5251fc48ba677
uh <carlos.uh@gmail.com>
2015-09-24 23:15:10 -0300

    This works? Ok.

--- accounts/src/main/java/accounts/Application.java
+++ accounts/src/main/java/accounts/Application.java
@@ -6,14 +6,12 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.hateoas.HypermediaAutoConfiguration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = { HypermediaAutoConfiguration.class })
 // @ComponentScan(basePackages = { "accounts" }, excludeFilters = { @Filter(type
 // = FilterType.ASSIGNABLE_TYPE, value = MockBeansConfiguration.class) })
 @PropertySource("classpath:/accounts/application.properties")
-@EnableRedisHttpSession
 public class Application implements CommandLineRunner {
 
 	@Override
---
+++ accounts/src/main/java/accounts/config/HttpSessionConfig.java
@@ -0,0 +1,10 @@
+package accounts.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@Configuration
+@EnableRedisHttpSession
+public class HttpSessionConfig {
+
+}
