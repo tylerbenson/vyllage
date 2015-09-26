208ce701a6bafc939e8b5b1f52099ebd74ef0216
uh <carlos.uh@gmail.com>
2015-09-26 02:01:05 -0300

    Added profile for session config.

--- accounts/src/main/java/accounts/config/HttpSessionConfig.java
+++ accounts/src/main/java/accounts/config/HttpSessionConfig.java
@@ -1,10 +1,13 @@
 package accounts.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
 @EnableRedisHttpSession
+@Profile(value = "dev")
+// TODO: change to prod.
 public class HttpSessionConfig {
 
 }
