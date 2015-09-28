8cb9cce93fb90036c8e334f15347273a4df32d29
uh <carlos.uh@gmail.com>
2015-09-28 00:12:41 -0300

    Set Redis configuration to be active on prod environment.

--- accounts/src/main/java/accounts/config/HttpSessionConfig.java
+++ accounts/src/main/java/accounts/config/HttpSessionConfig.java
@@ -2,11 +2,10 @@ package accounts.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
 @EnableRedisHttpSession
-@Profile(value = "dev")
+@Profile(value = "prod")
 // TODO: change to prod.
 public class HttpSessionConfig {
 
