e8b52f5da305da46ed269c01af9ab1e2ca17d601
uh <carlos.uh@gmail.com>
2015-09-28 09:49:14 -0300

    Readded dissapearing import.

--- accounts/src/main/java/accounts/config/HttpSessionConfig.java
+++ accounts/src/main/java/accounts/config/HttpSessionConfig.java
@@ -2,6 +2,7 @@ package accounts.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
 @EnableRedisHttpSession
