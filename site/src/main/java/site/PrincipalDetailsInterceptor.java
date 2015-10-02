1c5606d2437ce8199811c47b78ee53bebee5088d
uh <carlos.uh@gmail.com>
2015-10-02 00:36:55 -0300

    Added query on last date modified for document.

--- site/src/main/java/site/PrincipalDetailsInterceptor.java
+++ site/src/main/java/site/PrincipalDetailsInterceptor.java
@@ -4,6 +4,7 @@ import java.security.Principal;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 
+import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -12,6 +13,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
+import site.service.DocumentService;
 import user.common.User;
 import user.common.UserOrganizationRole;
 
@@ -19,6 +21,13 @@ import com.newrelic.api.agent.NewRelic;
 
 @Component
 public class PrincipalDetailsInterceptor extends HandlerInterceptorAdapter {
+
+	private final DocumentService documentService;
+
+	@Inject
+	public PrincipalDetailsInterceptor(final DocumentService documentService) {
+		this.documentService = documentService;
+	}
 
 	@Override
 	public boolean preHandle(HttpServletRequest request,
@@ -33,23 +42,31 @@ public class PrincipalDetailsInterceptor extends HandlerInterceptorAdapter {
 				NewRelic.addCustomParameter("date-last-access", request
 						.getSession(false).getCreationTime());
 
-			if (userPrincipal instanceof User) {
-				NewRelic.addCustomParameter("userId",
-						((User) userPrincipal).getUserId());
-
-				NewRelic.addCustomParameter("date-created-unix", this
-						.getUserDateUnix(((User) userPrincipal)
-								.getDateCreated()));
-
-				NewRelic.addCustomParameter("date-modified-unix", this
-						.getUserDateUnix(((User) userPrincipal)
-								.getLastModified()));
-			}
-
 			if (userPrincipal instanceof UsernamePasswordAuthenticationToken) {
+				UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) userPrincipal;
+
+				if (token.getPrincipal() instanceof User) {
+					User user = (User) token.getPrincipal();
+
+					NewRelic.addCustomParameter("userId", user.getUserId());
+
+					Long userDocumentLastModification = documentService
+							.getUserDocumentLastModification(request,
+									user.getUserId());
+
+					System.out.println(userDocumentLastModification);
+
+					NewRelic.addCustomParameter("date-document-last-modified",
+							userDocumentLastModification);
+
+					NewRelic.addCustomParameter("date-created-unix",
+							this.getUserDateUnix(user.getDateCreated()));
+
+					NewRelic.addCustomParameter("date-modified-unix",
+							this.getUserDateUnix(user.getLastModified()));
+				}
 
 				int i = 0;
-				UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) userPrincipal;
 
 				for (GrantedAuthority grantedAuthority : token.getAuthorities()) {
 
