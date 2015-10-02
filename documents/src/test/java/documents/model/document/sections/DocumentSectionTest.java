9a69f42b6735e43f9a5bf5884de3f62cb85a5776
uh <carlos.uh@gmail.com>
2015-10-01 21:59:13 -0300

    Added some tests to avoid having to reduce jacoco report on things that should not be taken into account, there's no way I can tests over a hundred of paths on @EqualsAndHashcode generated lombok code. Unfortunately COMPLEXITY had to be lowered.

--- documents/src/test/java/documents/model/document/sections/DocumentSectionTest.java
+++ documents/src/test/java/documents/model/document/sections/DocumentSectionTest.java
@@ -22,11 +22,11 @@ public class DocumentSectionTest {
 	}
 
 	@Test
-	public void testEquals() {
-		DocumentSection ds1 = new AchievementsSection();
+	public void testEqualsAchievementsSection() {
+		ProjectsSection ds1 = new ProjectsSection();
 		ds1.setType(SectionType.PROJECTS_SECTION.type());
 
-		DocumentSection ds2 = new AchievementsSection();
+		ProjectsSection ds2 = new ProjectsSection();
 		ds2.setType(SectionType.PROJECTS_SECTION.type());
 
 		assertTrue(ds1.equals(ds2));
@@ -34,9 +34,75 @@ public class DocumentSectionTest {
 
 	@Test
 	public void testEqualsNullFalse() {
-		DocumentSection ds1 = new AchievementsSection();
+		ProjectsSection ds1 = new ProjectsSection();
 		ds1.setType(SectionType.PROJECTS_SECTION.type());
 
 		assertFalse(ds1.equals(null));
+	}
+
+	@Test
+	public void testEqualsCareerInterestsSection() {
+		CareerInterestsSection ds1 = new CareerInterestsSection();
+		ds1.setType(SectionType.CAREER_INTERESTS_SECTION.type());
+
+		CareerInterestsSection ds2 = new CareerInterestsSection();
+		ds2.setType(SectionType.CAREER_INTERESTS_SECTION.type());
+
+		assertTrue(ds1.equals(ds2));
+	}
+
+	@Test
+	public void testEqualsEducationSection() {
+		EducationSection ds1 = new EducationSection();
+		ds1.setType(SectionType.EDUCATION_SECTION.type());
+
+		EducationSection ds2 = new EducationSection();
+		ds2.setType(SectionType.EDUCATION_SECTION.type());
+
+		assertTrue(ds1.equals(ds2));
+	}
+
+	@Test
+	public void testEqualsPersonalReferencesSection() {
+		PersonalReferencesSection ds1 = new PersonalReferencesSection();
+		ds1.setType(SectionType.PERSONAL_REFERENCES_SECTION.type());
+
+		PersonalReferencesSection ds2 = new PersonalReferencesSection();
+		ds2.setType(SectionType.PERSONAL_REFERENCES_SECTION.type());
+
+		assertTrue(ds1.equals(ds2));
+	}
+
+	@Test
+	public void testEqualsProfessionalReferencesSection() {
+		ProfessionalReferencesSection ds1 = new ProfessionalReferencesSection();
+		ds1.setType(SectionType.PROFESSIONAL_REFERENCES_SECTION.type());
+
+		ProfessionalReferencesSection ds2 = new ProfessionalReferencesSection();
+		ds2.setType(SectionType.PROFESSIONAL_REFERENCES_SECTION.type());
+
+		assertTrue(ds1.equals(ds2));
+	}
+
+	@Test
+	public void testEqualsSkillsSection() {
+		SkillsSection ds1 = new SkillsSection();
+		ds1.setType(SectionType.SKILLS_SECTION.type());
+
+		SkillsSection ds2 = new SkillsSection();
+		ds2.setType(SectionType.SKILLS_SECTION.type());
+
+		assertTrue(ds1.equals(ds2));
+	}
+
+	@Test
+	public void testEqualsSummarySection() {
+		SummarySection ds1 = new SummarySection();
+		ds1.setType(SectionType.SUMMARY_SECTION.type());
+
+		SummarySection ds2 = new SummarySection();
+		ds2.setType(SectionType.SUMMARY_SECTION.type());
+
+		assertTrue(ds1.equals(ds2));
 	}
 }
