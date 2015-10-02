50671a712ee7347ca0ddc0ac116cffa7ac68b223
Aaron Noel De Leon <aaronnoeldeleon@gmail.com>
2015-10-02 23:43:07 +0800

    Added Start Date and End Date on Diff

--- assets/src/components/resume/advices/index.jsx
+++ assets/src/components/resume/advices/index.jsx
@@ -11,8 +11,7 @@ var Advices = React.createClass({
   render: function () {
     var section = this.props.section || {};
 
-    if( this.props.section.advices != undefined ){
-
+    if( this.props.section.advices !== undefined ){
       var showHighlights = function( highlight ,index){
         return(
           <li className="highlight"><Diff key={index} inputA={section.highlights[index]} inputB={highlight} type="words" /></li>
@@ -26,7 +25,14 @@ var Advices = React.createClass({
       }.bind(this);
 
       var adviceList = this.props.section.advices.map(function(advice ,index){
-        if( advice.status == 'pending' )
+        if(section.type === 'EducationSection' || section.type === 'JobExperienceSection') {
+          var startDateA = section.startDate ? section.startDate : '';
+          var startDateB = advice.documentSection.startDate ? advice.documentSection.startDate : '';
+          var endDateA = section.endDate ? section.endDate : section.isCurrent ? 'Present' : '';
+          var endDateB = advice.documentSection.endDate ? advice.documentSection.endDate : advice.documentSection.isCurrent ? 'Present' : '';
+        }
+
+        if( advice.status == 'pending' ) {
         return <div key={index} className='comment'>
                 <div className='advice-content'>
                     { this.props.owner ? <div className="actions">
@@ -42,20 +48,22 @@ var Advices = React.createClass({
                       </div>
                     </div>
                     <div className="message">
-                      { section.type == 'SummarySection' ? <div>
+                      { section.type === 'SummarySection' ? <div>
                         <Diff inputA={section.description} inputB={advice.documentSection.description} type="words" />
                       </div>: null}
 
-                      { section.type == 'EducationSection' || section.type == 'JobExperienceSection' ? <div>
+                      { section.type === 'EducationSection' || section.type === 'JobExperienceSection' ? <div>
                         <h2><Diff inputA={section.organizationName} inputB={advice.documentSection.organizationName} type="words" /></h2>
                         <Diff inputA={section.organizationDescription} inputB={advice.documentSection.organizationDescription} type="words" />
                         <h3><Diff inputA={section.role} inputB={advice.documentSection.role} type="words" /></h3>
                         <Diff inputA={section.roleDescription} inputB={advice.documentSection.roleDescription} type="words" /> <br/>
+                        <Diff className="start date" inputA={startDateA} inputB={startDateB} type="words" /> -
+                        <Diff className="end date" inputA={endDateA} inputB={endDateB} type="words" />
                         <Diff inputA={section.location} inputB={advice.documentSection.location} type="words" />
                         <ul className="highlights">{advice.documentSection.highlights != undefined ? advice.documentSection.highlights.map(showHighlights) : null }</ul>
                       </div>: null}
 
-                      { section.type == 'SkillsSection' || section.type == 'CareerInterestsSection' ? <div>
+                      { section.type === 'SkillsSection' || section.type === 'CareerInterestsSection' ? <div>
                         <div className="tags">{advice.documentSection.tags != undefined ? advice.documentSection.tags.map(showTags) : null}</div>
                       </div>: null}
 
@@ -64,6 +72,7 @@ var Advices = React.createClass({
                   </div>
                 </div>
               </div>
+            }
       }.bind(this));
     }
 
