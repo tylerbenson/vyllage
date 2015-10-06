33d4a2f9ef18aa08b275371c24e76dd6f4a4a4c8
uh <uh@uh-laptop>
2015-10-05 23:20:46 -0300

    Test 3

--- accounts/src/main/java/accounts/config/CustomSocialConfiguration.java
+++ accounts/src/main/java/accounts/config/CustomSocialConfiguration.java
@@ -61,7 +61,7 @@ public class CustomSocialConfiguration extends SocialConfigurerAdapter {
 
 		ConnectControllerWithRedirect connectController = new ConnectControllerWithRedirect(
 				connectionFactoryLocator, connectionRepository);
-		// test2
+		// test3
 		connectController.setApplicationUrl("https://staging.vyllage.com");
 
 		connectController
