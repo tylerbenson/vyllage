1491617c9ab095509ea4d6c14aad29456dab85df
Carlos Gagliardi <carlos.uh@gmail.com>
2015-10-09 20:23:29 -0300

    Revert "Moved validator."
    This reverts commit d7251ded5ab1fa8e8746223593768a87edd9a338.

--- documents/src/main/java/documents/utilities/OrderSectionValidator.java
+++ documents/src/main/java/documents/utilities/OrderSectionValidator.java
@@ -1,4 +1,4 @@
-package documents.services.rules;
+package documents.utilities;
 
 import java.util.List;
 import java.util.logging.Logger;
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Component;
 import com.newrelic.api.agent.NewRelic;
 
 import documents.model.document.sections.DocumentSection;
-import documents.utilities.FindDuplicates;
 
 @Component
 public class OrderSectionValidator {
