bb402e1b81849a1662a7c16e527a89a7d97280d3
uh <carlos.uh@gmail.com>
2015-10-28 18:57:33 -0300

    Now with connections.

--- connections/src/test/java/connections/email/EmailTest.java
+++ connections/src/test/java/connections/email/EmailTest.java
@@ -2,6 +2,7 @@ package connections.email;
 
 import javax.inject.Inject;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -23,6 +24,13 @@ public class EmailTest {
 
 	// Replace this with your account to see the email.
 	private String to = "no-reply@vyllage.com";
+
+	@BeforeClass
+	public static void init() {
+		System.setProperty("spring.thymeleaf.prefix",
+				"file:///" + System.getProperty("PROJECT_HOME")
+						+ "/assets/src/");
+	}
 
 	@Test
 	public void test() {
