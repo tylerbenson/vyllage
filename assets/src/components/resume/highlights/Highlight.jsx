21511f0da23b8460ede26a76cd217f5a0b0a6fa8
Tareq Jobayere <tarexyz@gmail.com>
2015-11-17 16:56:09 +0600

    added contenteditable

--- assets/src/components/resume/highlights/Highlight.jsx
+++ assets/src/components/resume/highlights/Highlight.jsx
@@ -1,5 +1,6 @@
 var React = require('react');
 var Textarea = require('react-textarea-autosize');
+var ContentEditable = require("react-contenteditable");
 
 var Highlight = React.createClass({
 	render: function() {
@@ -7,15 +8,14 @@ var Highlight = React.createClass({
       return null;
     }
 		return (
-			<li className="highlight">
-				<span className="flat">{this.props.text.replace(/\n{3,}/,'\n\n')}</span>
-				{this.props.uiEditMode ?
-					<button className="flat icon small secondary" onClick={this.props.onDelete}>
-						<i className="ion-trash-a"></i>
-					</button>
-				: null}
+			<li className="highlight flat">
+				<ContentEditable html={this.props.text.replace(/\n{3,}/,'\n\n')} onChange={this._handleEdit}  />
 			</li>
 		);
+	},
+
+	_handleEdit : function(e){
+		
 	}
 
 });
