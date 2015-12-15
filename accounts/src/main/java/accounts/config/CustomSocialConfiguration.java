f10e07d88f8df15bb97d0c714d5c0b2ecb75fb9b
uh <carlos.uh@gmail.com>
2015-12-15 18:00:06 -0300

    Fixes https://github.com/natebenson/vyllage/issues/1221. Fixes https://github.com/natebenson/vyllage/issues/1222.
    Upgraded Spring Social from 1.1.2 to 1.1.4.
    Upgraded Jackson from 2.5.4 to 2.5.5.
    Upgraded Spring Social Facebook from 2.0.1 to 2.0.3.

--- accounts/src/main/java/accounts/config/CustomSocialConfiguration.java
+++ accounts/src/main/java/accounts/config/CustomSocialConfiguration.java
@@ -1,7 +1,10 @@
 package accounts.config;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.util.logging.Logger;
 
+import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.sql.DataSource;
 
@@ -61,6 +64,46 @@ public class CustomSocialConfiguration extends SocialConfigurerAdapter {
 				dataSource, connectionFactoryLocator, textEncryptor);
 		jdbcUsersConnectionRepository.setTablePrefix("ACCOUNTS.");
 		return jdbcUsersConnectionRepository;
+	}
+
+	/**
+	 * http://stackoverflow.com/questions/34125425/spring-social-facebook-login-
+	 * error-numeric-value-out-of-range-of-int <br>
+	 * This is fixed in Spring Social Facebook 2.0.3 but Jackson insists the
+	 * field 'size' is an int in VideoUploadLimits even though is long. Changing
+	 * Jackson versions does not fix it. <br>
+	 * Since we don't need videos right now I'll remove the field.
+	 */
+	@PostConstruct
+	private void init() {
+		// hack for the facebook login
+		try {
+			String[] fieldsToMap = { "id", "about", "age_range", "address",
+					"bio", "birthday", "context", "cover", "currency",
+					"devices", "education", "email", "favorite_athletes",
+					"favorite_teams", "first_name", "gender", "hometown",
+					"inspirational_people", "installed", "install_type",
+					"is_verified", "languages", "last_name", "link", "locale",
+					"location", "meeting_for", "middle_name", "name",
+					"name_format", "political", "quotes",
+					"payment_pricepoints", "relationship_status", "religion",
+					"security_settings", "significant_other", "sports",
+					"test_group", "timezone", "third_party_id", "updated_time",
+					"verified", "viewer_can_send_gift", "website", "work" };
+
+			Field field = Class.forName(
+					"org.springframework.social.facebook.api.UserOperations")
+					.getDeclaredField("PROFILE_FIELDS");
+			field.setAccessible(true);
+
+			Field modifiers = field.getClass().getDeclaredField("modifiers");
+			modifiers.setAccessible(true);
+			modifiers.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+			field.set(null, fieldsToMap);
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 	}
 
 	@Bean
