2b828e2a465927cef812b9367baf0509c00f860f
uh <carlos.uh@gmail.com>
2016-02-01 15:39:08 -0300

    Removed unnecessary method.

--- documents/src/main/java/documents/model/document/sections/Mergeable.java
+++ documents/src/main/java/documents/model/document/sections/Mergeable.java
@@ -2,8 +2,6 @@ package documents.model.document.sections;
 
 public interface Mergeable {
 
-	public String getType();
-
 	/**
 	 * Merges sections of the same type. Replaces properties of the first
 	 * sections with ones from the other.
