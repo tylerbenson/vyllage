69a729e92a3027b45beb5b100d4d4f45e68556c1
uh <carlos.uh@gmail.com>
2016-02-03 02:35:56 -0300

    Cleaned up a bit.

--- documents/src/main/java/documents/services/rezscore/RezscoreService.java
+++ documents/src/main/java/documents/services/rezscore/RezscoreService.java
@@ -54,38 +54,40 @@ public class RezscoreService {
 	public Optional<RezscoreResult> getRezscoreAnalysis(
 			DocumentHeader documentHeader,
 			List<DocumentSection> documentSections) {
-		HttpEntity<String> header = assembleHeader();
-		StringBuilder sb = new StringBuilder();
 
 		if (documentHeader == null || documentSections == null
 				|| documentSections.isEmpty())
 			return Optional.empty();
 
-		final Long documentId = documentSections.stream()
-				.map(ds -> ds.getDocumentId()).collect(Collectors.toList())
-				.get(0);
+		final Long documentId = getDocumentId(documentSections);
 
 		// sort by position
-		documentSections.sort((s1, s2) -> s1.getSectionPosition().compareTo(
-				s2.getSectionPosition()));
+		sortSections(documentSections);
 
 		// create txt
-		sb.append(documentHeader.asTxt()).append("\n");
-
-		for (DocumentSection documentSection : documentSections) {
-			sb.append(documentSection.asTxt());
-			sb.append("\n");
-		}
-
-		final String resume = sb.toString();
+		final String resume = createTxtResume(documentHeader, documentSections);
 
 		// logger.info(resume);
+
+		RezscoreResult rezscoreResult = getRezcoreResult(documentId, resume);
+
+		return Optional.ofNullable(rezscoreResult);
+	}
+
+	/**
+	 * Returns Rezscore analysis.
+	 * 
+	 * @param documentId
+	 * @param resume
+	 * @return
+	 */
+	protected RezscoreResult getRezcoreResult(final Long documentId,
+			final String resume) {
+		HttpEntity<String> header = assembleHeader();
 
 		String url = getUrl(resume);
 
 		RezscoreResult rezscoreResult = null;
-
-		// get from cache
 		try {
 			rezscoreResult = results
 					.get(documentId,
@@ -106,10 +108,59 @@ public class RezscoreService {
 			logger.severe(ExceptionUtils.getStackTrace(e));
 			NewRelic.noticeError(e);
 		}
-
-		return Optional.ofNullable(rezscoreResult);
+		return rezscoreResult;
 	}
 
+	/**
+	 * Creates a plain text resume based on the document header and document
+	 * sections.
+	 * 
+	 * @param documentHeader
+	 * @param documentSections
+	 * @return plain text resume
+	 */
+	protected String createTxtResume(DocumentHeader documentHeader,
+			List<DocumentSection> documentSections) {
+
+		StringBuilder sb = new StringBuilder();
+		sb.append(documentHeader.asTxt()).append("\n");
+
+		for (DocumentSection documentSection : documentSections) {
+			sb.append(documentSection.asTxt());
+			sb.append("\n");
+		}
+
+		final String resume = sb.toString();
+		return resume;
+	}
+
+	/**
+	 * Sort sections by sectionPosition.
+	 * 
+	 * @param documentSections
+	 */
+	protected void sortSections(List<DocumentSection> documentSections) {
+		documentSections.sort((s1, s2) -> s1.getSectionPosition().compareTo(
+				s2.getSectionPosition()));
+	}
+
+	/**
+	 * Get document id from the sections.
+	 * 
+	 * @param documentSections
+	 * @return
+	 */
+	protected Long getDocumentId(List<DocumentSection> documentSections) {
+		return documentSections.stream().map(ds -> ds.getDocumentId())
+				.collect(Collectors.toList()).get(0);
+	}
+
+	/**
+	 * Creates Rezscore url endpoint.
+	 * 
+	 * @param resume
+	 * @return
+	 */
 	protected String getUrl(String resume) {
 		UriComponentsBuilder builder = UriComponentsBuilder.newInstance();
 
