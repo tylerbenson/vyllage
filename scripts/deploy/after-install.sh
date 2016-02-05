1b9e4a436b7c0144e79e83b0b0cece372b7f0f33
Tyler Benson <tylerbenson@gmail.com>
2016-02-04 23:11:23 -0800

    Linking agent no longer needed.

--- scripts/deploy/after-install.sh
+++ scripts/deploy/after-install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -ex
 
 sudo chmod 777 /opt/vyllage/newrelic
-ln -s /opt/vyllage/site-*.jar /opt/vyllage/site.jar
-ln -s /opt/vyllage/newrelic/newrelic-agent-*.jar /opt/vyllage/newrelic/newrelic.jar
+# ln -s /opt/vyllage/site-*.jar /opt/vyllage/site.jar
+# ln -s /opt/vyllage/newrelic/newrelic-agent-*.jar /opt/vyllage/newrelic/newrelic.jar
 
 REV=`aws deploy list-application-revisions --application-name=vyllage --sort-by lastUsedTime --sort-order descending --region us-west-2 | grep "key" | head -n 1 | sed -r 's/.*"vyllage-(.+)\.zip"/\1/'`
 java -jar /opt/vyllage/newrelic/newrelic.jar deployment --revision=$REV & # Probably don't want this holding up the deploy.
