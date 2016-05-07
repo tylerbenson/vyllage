c4b72e4a98df17a43b09894fc08f2a0569c9b8ed
Tyler Benson <tylerbenson@gmail.com>
2016-05-07 16:20:12 -0700

    Update content and request demo instead of new account.

--- assets/src/homepage.jsx
+++ assets/src/homepage.jsx
@@ -1,8 +1,8 @@
 var React = require('react');
 var LoginButton = require('./components/buttons/login');
-var RegisterButton = require('./components/buttons/register');
+// var RegisterButton = require('./components/buttons/register');
 
 React.initializeTouchEvents(true);
 React.render(<LoginButton />, document.getElementById('login'));
-React.render(<RegisterButton />, document.getElementById('header-register'));
-React.render(<RegisterButton />, document.getElementById('footer-register'));
+// React.render(<RegisterButton />, document.getElementById('header-register'));
+// React.render(<RegisterButton />, document.getElementById('footer-register'));
